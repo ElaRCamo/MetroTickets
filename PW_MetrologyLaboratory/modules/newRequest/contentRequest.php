e38871de990ecc951732de942febdf0031f5cd00
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-02-22 12:32:04 -0600

    Correccion new request-Se revierte Merge remote-tracking branch 'origin/main'

--- PW_MetrologyLaboratory/modules/newRequest/contentRequest.php
+++ PW_MetrologyLaboratory/modules/newRequest/contentRequest.php
@@ -1,8 +1,8 @@
 <main>
     <div class="page-header row headerLogo">
         <div class="col divTitle">
-            <h1> Nueva Solicitud </h1>
-            <small>Favor de registrar los datos para la nueva solicitud</small>
+            <h1> Solicitar una prueba </h1>
+            <small>Favor de registrar los datos siguientes:</small>
         </div>
         <div class="logoRight col-sm-3">
             <div>
@@ -13,205 +13,70 @@
             </div>
         </div>
     </div>
-
-    <div class="page-content table-responsive-sm newRequestTabla">
-        <div class="container-fluid table-responsive">
-            <form name="formNewRequest" action="" method="POST">
-                <table class="table table-borderless tableOpe table-responsive">
-                    <!--Fila uno-->
-                    <thead>
-                        <tr class="table-borderless">
-                            <th>
-                                <div class="form-group">
-                                    <label for="folio">FOLIO:<br>
-                                        <input type="number" class="form-control" id="idFolio" name="Folio" placeholder="6097" readonly>
-                                    </label>
+    <section id="request-form-section" class="form-content-wrap page-content">
+        <div class=" container">
+            <div class=" row">
+                <div class="tab-content">
+                    <div class="col-sm-12">
+                        <div class="item-wrap">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form name="formNewRequest" action="" method="POST" id="formRequestLab" data-toggle="validator" class="popup-form">
+                                        <div class="row">
+                                            <div id="msgContactSubmit" class="hidden"></div>
+                                            <div class="form-group col-sm-6">
+                                                <div class="help-block with-errors"></div>
+                                                <input type="text" class="form-control" id="numParte" placeholder="Número de parte" required data-error="Por favor ingresa el número de parte">
+                                                <div class="input-group-icon"><i class="las la-cog"></i></div>
+                                            </div><!-- end form-group -->
+                                            <div class="form-group col-sm-6">
+                                                <div class="help-block with-errors"></div>
+                                                <input type="text" class="form-control" id="OEM" placeholder="OEM" required data-error="Por favor ingresa OEM">
+                                                <div class="input-group-icon"><i class="las la-screwdriver"></i></div>
+                                            </div><!-- end form-group -->
+                                            <div class="form-group col-sm-6">
+                                                <div class="help-block with-errors"></div>
+                                                <input type="text" class="form-control" id="plataforma" placeholder="Plataforma" required data-error="Por favor ingresa la plataforma">
+                                                <div class="input-group-icon"><i class="las la-warehouse"></i></div>
+                                            </div><!-- end form-group -->
+                                            <div class="form-group col-sm-6">
+                                                <div class="help-block with-errors"></div>
+                                                <select class="form-control" id="tipoPrueba" name="tiposPrueba" title="TipoDePrueba" required data-error="Por favor seleccione tipo de prueba">
+                                                    <option value="" disabled selected>Seleccione el tipo de prueba</option>
+                                                    <option value="dimensional">Dimensional</option>
+                                                    <option value="full">Full</option>
+                                                    <option value="maquinaUniversal">Máquina Universal</option>
+                                                </select>
+                                                <div class="input-group-icon"><i class="las la-ruler-combined"></i></div>
+                                            </div><!-- end form-group -->
+                                            <!-- Formulario dependiendo tipo de prueba -->
+                                            <div id="dimensionalFields" class="form-group col-sm-12" style="display: none;">
+                                                <div class="help-block with-errors"></div>
+                                                <textarea rows="3" name="campoDimensional" id="campoDimensional" placeholder="Observaciones Dimensional*" class="form-control" required data-error="Por favor ingresa tus observaciones"></textarea>
+                                                <div class="textarea las la-clipboard-check"><i class="fa fa-pencil"></i></div>
+                                            </div>
+                                            <div id="fullFields" class="form-group" style="display: none;">
+                                                <textarea rows="3" name="campoFull" id="campoFull" placeholder="Observaciones Full*" class="form-control" required data-error="Por favor ingresa tus observaciones"></textarea>
+                                                <div class="textarea las la-clipboard-check"><i class="fa fa-pencil"></i></div>
+                                            </div>
+                                            <div id="maquinaUniversalFields" class="form-group" style="display: none;">
+                                                <textarea rows="3" name="campoMaquinaUniversal" id="campoMaquinaUniversal" placeholder="Observaciones Máquina Universal*" class="form-control" required data-error="Por favor ingresa tus observaciones"></textarea>
+                                                <div class="textarea las la-clipboard-check"><i class="fa fa-pencil"></i></div>
+                                            </div>
+                                            <div class="form-group last col-sm-12">
+                                                <button type="button" id="submit" class="btn btn-custom"><i class='las la-paper-plane'></i> Enviar</button>
+                                            </div><!-- end form-group -->
+                                            <div class="sub-text">* Campos requeridos</div>
+                                            <div class="clearfix"></div>
+                                        </div><!-- end row -->
+                                    </form><!-- end form -->
                                 </div>
-                            </th>
-                            <th>
-                                <div class="form-group">
-                                    <label for="fecha">FECHA:
-                                        <input type="date" class="form-control" id="idfecha" name="fecha" readonly>
-                                    </label>
-                                </div>
-                            </th>
-                            <th>
-                                <div class="form-group">
-                                    <label for="solicitante">SOLICITADO POR:
-                                        <input type="text" class="form-control" id="idSolicitante" name="solicitante" placeholder="Aracely" readonly>
-                                    </label>
-                                </div>
-                            </th>
-                            <th>
-                                <div class="form-group">
-                                    <label for="turno">TURNO:
-                                        <input type="text" class="form-control" id="idTurno" name="turno" placeholder="1°" readonly>
-                                    </label>
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <!--Fila dos-->
-                    <tr>
-                        <td>
-                            <div class="form-group">
-                                <label for="area">ÁREA/DEPTO:
-                                    <input type="text" class="form-control" id="idArea" name="area" required>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="linea">LÍNEA:<br>
-                                    <input type="text" class="form-control" id="idLinea" name="linea" required>
-                                </label>
-
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="estacion">ESTACIÓN:
-                                    <input type="text" class="form-control" id="idEstacion" name="estacion" required>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="mantenimiento">MANTENIMIENTO:<br>
-                                    <input type="radio" name="mantenimiento" value="correctivo" required>Correctivo
-                                    <input type="radio" name="mantenimiento" value="preventivo" required>Preventivo
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                    <!--Fila tres-->
-                    <tr>
-                        <td colspan="3" >
-                            <div class="form-group">
-                                <label for="problema">DESCRIPCIÓN DEL PROBLEMA/TRABAJO:
-                                    <input type="text" class="form-control" id="idProblema" name="problema" required>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="maquina">ESTATUS MÁQUINA:<br>
-                                    <input type="radio" name="maquina" value="trabaja" required>Trabaja
-                                    <input type="radio" name="maquina" value="noTrabaja" required>No trabaja
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-                <div class="div-button">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="confirmarInfo">
-                        <label class="form-check-label" for="confirmarInfo">Confirmo que la información es correcta</label>
-                    </div>
-                    <br><button type="submit" class="btn btn-primary">Generar Solicitud</button>
-                </div>
-            </form>
-            <div class="col">
-                <h6>La siguiente información es llenada por el técnico</h6>
-            </div>
-            <form name="formNewRequest" action="" method="POST">
-                <table class="table table-borderless tableTec">
-                    <!--Fila uno-->
-                    <tr>
-                        <td >
-                            <div class="form-group">
-                                <label for="ejecutadoPor">EJECUTADO POR EL TÉCNICO:
-                                    <input type="text" class="form-control" id="idEjecutadoPor" name="ejecutadoPor" placeholder="Cristopher Mtz." readonly>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="horaRecibo">HORA RECIBO:
-                                    <input type="time" class="form-control" id="idHoraRecibo" name="horaRecibo" readonly>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="afectados">OPERADORES AFECTADOS:
-                                    <input type="number" class="form-control" id="idAfectados" name="afectados">
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                    <!--Fila dos-->
-                    <tr>
-                        <td colspan="2" >
-                            <div class="form-group">
-                                <label for="diagnostico">DIAGNÓSTICO/TRABAJO REALIZADO:
-                                    <input type="text" class="form-control" id="idDiagnostico" name="diagnostico" required>
-                                </label>
-                            </div>
-                        </td>
-                        <td rowspan="2">
-                            <div class="form-group">
-                                <label for="obsTecnico">OBSERVACIONES:
-                                    <input type="text" class="form-control" id="idObsTecnico" name="obsTecnico" required>
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                    <!--Fila tres-->
-                    <tr>
-                        <td colspan="2">
-                            <div class="form-group">
-                                <label for="causa">CAUSA RAÍZ:
-                                    <input type="text" class="form-control" id="idCausa" name="causa" required>
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-                <div class="div-button">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="confirmarInfo">
-                        <label class="form-check-label" for="confirmarInfo">Confirmo que la información es correcta</label>
-                    </div>
-                    <br><button type="submit" class="btn btn-primary">Registrar Solitud</button>
-                </div>
-            </form>
-
-            <form name="formNewRequestCierre" action="" method="POST">
-                <div class="col">
-                    <h6>Favor de registrar el cierre de la solicitud.</h6>
-                </div>
-                <table class="table table-borderless tableTec">
-                    <tr>
-                        <td colspan="2">
-                            <div class="form-group">
-                                <label for="aprobadoPor">RECIBIDO Y APROBADO POR:
-                                    <input type="text" class="form-control" id="idAprobadoPor" name="aprobadoPor" placeholder="Aracely" readonly>
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="calificacion">CALIFICACIÓN DE TRABAJO RECIBIDO:<br>
-                                    <input type="radio" name="calificacion" value="excelente" required>Excelente
-                                    <input type="radio" name="calificacion" value="aceptable" required>Aceptable
-                                    <input type="radio" name="calificacion" value="noConforme" required>No conforme
-                                </label>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-group">
-                                <label for="observadciones">OBSERVACIONES:<br>
-                                    <input type="text" class="form-control" id="idObservaciones" name="observadciones" required>
-                                </label>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-                <div class="div-button">
-                    <br><button type="submit" class="btn btn-primary">Cerrar Solitud</button>
-                </div>
-            </form>
-        </div>
-    </div>
+                            </div><!--End row -->
+                            <!-- Popup end -->
+                        </div><!-- end item-wrap -->
+                    </div><!--End col -->
+                </div><!--End tab-content -->
+            </div><!--End row -->
+        </div><!--End container -->
+    </section>
 </main>
--- PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
+++ PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Nueva Solicitud</title>
 
@@ -13,7 +14,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;500;700&display=swap" rel="stylesheet">
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/contact-form.css">
     <link rel="stylesheet" href="../../css/style.css">
+
+    <!-- -Archivos de jQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
     <?php
@@ -28,21 +35,17 @@
     ?>
 
     <script>
-        // Get the container element
-        let listContainer = document.getElementById("menuLateral");
+        $(document).ready(function(){
+            // Manejar el cambio en el select
+            $("#tipoPrueba").change(function(){
+                // Ocultar todos los campos
+                $("#dimensionalFields, #fullFields, #maquinaUniversalFields").hide();
 
-        // Get all buttons with class="btn" inside the container
-        let lista = listContainer.getElementsByClassName("optionMenu");
-
-        // Loop through the buttons and add the active class to the current/clicked button
-        for (let i = 0; i < lista.length; i++) {
-            lista[i].addEventListener("click", function() {
-                let current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            });
-        }
+                // Mostrar el campo correspondiente a la opción seleccionada
+                var selectedOption = $(this).val();
+                $("#" + selectedOption + "Fields").show(); });
     </script>
+    <script src="../../js/general.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
