3c72a361d7737f3ff78f4411b4fcc4d045de6f58
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-04 11:43:00 -0600

    Update prueba_con.php

--- PW_MetrologyLaboratory/dao/prueba_con.php
+++ PW_MetrologyLaboratory/dao/prueba_con.php
@@ -3,12 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Incluye el archivo connection.php
+
 require_once('connection.php');
 
-// Crea una instancia de LocalConector
+
 $localConector = new LocalConector();
 
-// Llama a la función conectar
+
 $localConector->conectar();
 ?>
