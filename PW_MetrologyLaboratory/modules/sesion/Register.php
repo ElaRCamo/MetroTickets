2017eac91b1642afbab555082b5e8abea5532570
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-04 12:47:48 -0600

    Conexión a la BD correcta

--- PW_MetrologyLaboratory/modules/sesion/Register.php
+++ PW_MetrologyLaboratory/modules/sesion/Register.php
@@ -34,7 +34,7 @@
             </div>
         </div>
         <div class="wrapper wrapper-register">
-            <form id="registrarseForm" action="../dao/userRegister.php" method="POST">
+            <form id="registrarseForm" action="../../dao/userRegister.php" method="POST">
                 <h2 id="registrarse">Regístrarse</h2>
                 <div class="input-box">
                     <input type="text" id="nombreUsuario" name="nombreUsuario" placeholder="Nombre Completo" required>
