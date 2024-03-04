63b2e7b3be3238a1531afe8ef1e1ebfdcc54ca12
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-04 14:54:18 -0600

    Conexión a la BD

--- PW_MetrologyLaboratory/dao/connection.php
+++ PW_MetrologyLaboratory/dao/connection.php
@@ -2,15 +2,29 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 class LocalConector{
     private $host = "127.0.0.1:3306";
     private $usuario = "u543707098_MARIELA";
     private $clave = "Metrologia123";
     private $db = "u543707098_PRODUCCION";
+    private $conexion;
 
     public function conectar(){
-        $con = mysqli_connect($this->host,$this->usuario,$this->clave,$this->db);
-        return $con;
+        $this->conexion = mysqli_connect($this->host, $this->usuario, $this->clave, $this->db);
+        if ($this->conexion->connect_error) {
+            die("Error de conexión: " . $this->conexion->connect_error);
+        }
+    }
+
+    public function cerrarConexion() {
+        if ($this->conexion) {
+            $this->conexion->close();
+        }
+    }
+
+    public function getConexion() {
+        return $this->conexion;
     }
 }
 ?>
