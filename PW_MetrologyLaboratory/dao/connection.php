9530be247c2ae23458b00d205cf0140e676fff45
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-04 11:01:04 -0600

    prueba con

--- PW_MetrologyLaboratory/dao/connection.php
+++ PW_MetrologyLaboratory/dao/connection.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 class LocalConector{
     private $host = "127.0.0.1:3306";
     private $usuario = "u543707098_MARIELA";
