b1b7f62dd536520e8d7a242d1a806bee4600f254
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-04 11:13:05 -0600

    Pass

--- PW_MetrologyLaboratory/dao/connection.php
+++ PW_MetrologyLaboratory/dao/connection.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 class LocalConector{
     private $host = "127.0.0.1:3306";
     private $usuario = "u543707098_MARIELA";
-    private $clave = "Grammer1";
+    private $clave = "Metrologia1";
     private $db = "u543707098_PRODUCCION";
 
     public function conectar(){
