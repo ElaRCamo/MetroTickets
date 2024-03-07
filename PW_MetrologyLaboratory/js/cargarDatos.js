01e33c94c3e9543f7adb5641ae9c7332c9ab8486
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-07 09:44:46 -0600

    Se agrega daoPlataforma

--- PW_MetrologyLaboratory/js/cargarDatos.js
+++ PW_MetrologyLaboratory/js/cargarDatos.js
@@ -11,6 +11,9 @@ function llenarCliente(){
     });
 }
 function llenarPlataforma() {
+    var valorCliente = document.getElementById("cliente").value;
+    alert(valorCliente);
+
     $.getJSON('https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/dao/daoPlataforma.php?id_cliente=' + document.getElementById("cliente").value, function (data) {
         var selectS = document.getElementById("plataforma");
         selectS.innerHTML = ""; //limpiar contenido
