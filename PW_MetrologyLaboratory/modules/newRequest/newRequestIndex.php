d425767227e34af8a0705a7e5e3041c7ea9c15f7
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-07 12:00:56 -0600

    Se agrega icono de la ventana

--- PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
+++ PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../../imgs/Grammer_Logo.ico" type="image/x-icon">
     <title>Nueva Solicitud</title>
 
     <!--Enlace de iconos: icons8, licencia con mención -->
@@ -32,6 +33,9 @@
         # Content section
             require_once('../../footer.php');
     ?>
+
+    <script src="../../js/general.js"></script>
+    <script src="../../js/cargarDatos.js"></script>
     <script>
         $(document).ready(function() {
             $("#tipoEvaluacion").change(function() {
@@ -40,8 +44,6 @@
             });
         });
     </script>
-    <script src="../../js/general.js"></script>
-    <script src="../../js/cargarDatos.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
--- PW_MetrologyLaboratory/modules/settings/settingsIndex.php
+++ PW_MetrologyLaboratory/modules/settings/settingsIndex.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../../imgs/Grammer_Logo.ico" type="image/x-icon">
     <title>Panel Principal</title>
 
     <!--Enlace de iconos: icons8, licencia con mención -->
