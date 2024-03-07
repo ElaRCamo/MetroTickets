57e3a5361f81c074942a324f6144e2f9353313a2
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-07 14:11:46 -0600

    Se ajustan funciones js

--- PW_MetrologyLaboratory/js/general.js
+++ PW_MetrologyLaboratory/js/general.js
@@ -36,6 +36,17 @@ function llenarEvaluacion(){
 function banderaTipoEvaluacion(){
     if (cbTipoEva.value != null){
         divSelectTipoPrueba.style.display = "block";
+        $.getJSON('https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/dao/daoTipoPrueba.php?id_tipoEvaluacion=' + document.getElementById("tipoEvaluacion").value, function (data) {
+            var selectS = document.getElementById("tipoPrueba");
+            //selectS.innerHTML = ""; //limpiar contenido
+
+            for (var i = 0; i < data.data.length; i++) {
+                var createOptionS = document.createElement("option");
+                createOptionS.value = data.data[i].id_tipoPrueba;
+                createOptionS.text = data.data[i].descripcionPrueba;
+                selectS.appendChild(createOptionS);
+            }
+        });
     }else{
         divSelectTipoPrueba.style.display = "none";
     }
@@ -80,17 +91,7 @@ function banderaTipoPrueba(){
         divDetallesPrueba.style.display = "none";
 
     }
-    $.getJSON('https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/dao/daoTipoPrueba.php?id_tipoEvaluacion=' + document.getElementById("tipoEvaluacion").value, function (data) {
-        var selectS = document.getElementById("tipoPrueba");
-        //selectS.innerHTML = ""; //limpiar contenido
 
-        for (var i = 0; i < data.data.length; i++) {
-            var createOptionS = document.createElement("option");
-            createOptionS.value = data.data[i].id_tipoPrueba;
-            createOptionS.text = data.data[i].descripcionPrueba;
-            selectS.appendChild(createOptionS);
-        }
-    });
 }
 //document.getElementById("normaFile").onchange = function() {mostrarNombreArchivo()};
 function mostrarNombreArchivo() {
