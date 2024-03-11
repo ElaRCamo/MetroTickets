b0ec3ab7b37c42c9d689455cb315d5f6f942b90f
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-11 08:23:23 -0600

    userRegister Prueba

--- PW_MetrologyLaboratory/dao/userRegister.php
+++ PW_MetrologyLaboratory/dao/userRegister.php
@@ -1,6 +1,28 @@
 <?php
-include('connection.php');
+include_once('connection.php');
 
+$numNomina     = $_POST['numNomina'];
+$nombreUsuario = $_POST['nombreUsuario'];
+$correo        = $_POST['correo'];
+$password      = $_POST['password'];
+
+RegistrarUsuario($numNomina ,$nombreUsuario, $correo, $password);
+function RegistrarUsuario($numNomina ,$nombreUsuario, $correo, $password){
+    $con = new LocalConector();
+    $conex = $con->conectar();
+
+    $insertUsuario = "INSERT INTO `Usuario` (`id_usuario`, `nombreUsuario`, `correoElectronico`, `passwordHash`) VALUES ('$numNomina', '$nombreUsuario', '$correo', '$password')";
+
+    echo '<script>alert("' . $insertUsuario . '")</script>';
+    $rInsertUsuario = mysqli_query($conex,$insertUsuario);
+    echo 1;
+
+    mysqli_close($conex);
+
+    return $rInsertUsuario;
+}
+
+/*
 if (isset($_POST['registrarse'])){
     $numNomina     = $_POST['numNomina'];
     $nombreUsuario = $_POST['nombreUsuario'];
@@ -32,6 +54,6 @@ function RegistrarUsuario($numNomina ,$nombreUsuario, $correo, $password){
     //echo '<script>alert("Usuario registrado exitosamente"); window.location.href = "https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/Index.php";</script>';
     return 1;
 }
-
+*/
 ?>
 
