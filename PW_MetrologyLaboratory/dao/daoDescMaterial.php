23f204d1de096c5dc1ddc4630b46fd97310a4e48
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-14 09:10:38 -0600

    Se agrega cargarImagen() Prueba

--- PW_MetrologyLaboratory/dao/daoDescMaterial.php
+++ PW_MetrologyLaboratory/dao/daoDescMaterial.php
@@ -10,6 +10,7 @@ function contadorPlataforma($id_plataforma ){
     $conex = $con->conectar();
 
     $sqlDescMaterial =  mysqli_query($conex, "SELECT id_descripcion,descripcionMaterial FROM DescripcionMaterial WHERE id_plataforma='$id_plataforma' ORDER BY descripcionMaterial;");
+    echo '<script>alert ("$sqlDescMaterial"); </script>';
     $resultado= mysqli_fetch_all($sqlDescMaterial, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
 }
