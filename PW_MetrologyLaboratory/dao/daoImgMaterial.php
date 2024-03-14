200779195727354836c2ecee0ea7095681dad141
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-14 09:56:14 -0600

    Se agrega cargarImagen() Prueba

--- PW_MetrologyLaboratory/dao/daoImgMaterial.php
+++ PW_MetrologyLaboratory/dao/daoImgMaterial.php
@@ -12,7 +12,6 @@ function CargarImagen($id_descripcion){
     $datos = mysqli_query($conex, "SELECT imgMaterial FROM `DescripcionMaterial` WHERE id_descripcion='$id_descripcion';");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
-    echo '<script>alert("' . $resultado . '");</script>';
     echo json_encode(array("data" => $resultado));
 }
 
