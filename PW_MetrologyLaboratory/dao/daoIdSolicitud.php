f07fb03408c1666cb40f34d25363d343391f14e0
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-19 11:24:51 -0600

    Prueba idPrueba

--- PW_MetrologyLaboratory/dao/daoIdSolicitud.php
+++ PW_MetrologyLaboratory/dao/daoIdSolicitud.php
@@ -9,7 +9,7 @@ function ConsultaIdMax()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $sqlIdMax = mysqli_query($conex, "SELECT MAX(id_prueba) AS max_id_prueba FROM `SolicitudPrueba`");
+    $sqlIdMax = mysqli_query($conex, "SELECT MAX(id_prueba) AS max_id_prueba FROM `Prueba`");
 
     $resultado = mysqli_fetch_all($sqlIdMax, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
