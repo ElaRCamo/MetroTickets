a72776e1859b94616773cd8e55c0010a546f1e4d
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-19 14:34:17 -0600

    Correccion validacion para session_start();

--- PW_MetrologyLaboratory/index.php
+++ PW_MetrologyLaboratory/index.php
@@ -15,6 +15,17 @@
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <?php
+    session_start();
+
+    if ($_SESSION["nomina"] == "" && $_SESSION["nomina"]== null && $_SESSION["password"]== "" && $_SESSION["password"]== null) {
+        echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=index.html'>";
+        session_destroy();
+    }else{
+        session_start();
+    }
+    ?>
+
 </head>
 <body>
     <?php
