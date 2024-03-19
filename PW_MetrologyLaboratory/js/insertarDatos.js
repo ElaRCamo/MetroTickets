b51411b2836e6e7621dcb256c7a756eb4ec80836
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-19 11:40:23 -0600

    Prueba con espera a la llamada asincrona

--- PW_MetrologyLaboratory/js/insertarDatos.js
+++ PW_MetrologyLaboratory/js/insertarDatos.js
@@ -61,9 +61,11 @@ function obtenerNuevoId() {
         console.error("Error al obtener el nuevo ID:", error);
     });
 }
-
+/*
 
 function registrarSolicitud(){
+
+    var id_prueba = obtenerNuevoId();
 
     const dataForm = new FormData();
 
@@ -84,7 +86,6 @@ function registrarSolicitud(){
     var fechaFormateada = fechaSolicitud.getFullYear() + '-' + (fechaSolicitud.getMonth() + 1) + '-' + fechaSolicitud.getDate();
 
 
-
     dataForm.append('tipoPrueba', tipoPrueba.value.trim());
     dataForm.append('norma', norma.value.trim());
     dataForm.append('normaFile', normaFile.value.trim());
@@ -96,8 +97,9 @@ function registrarSolicitud(){
     dataForm.append('descMaterial', descMaterial.value.trim());
     dataForm.append('cdadMaterial', cdadMaterial.value.trim());
     dataForm.append('fechaSolicitud', fechaFormateada);
+    dataForm.append('id_prueba', id_prueba);
 
-    var id_prueba = obtenerNuevoId();
+
     /*
     idPrueba().then(function(nuevoId) {
         id_prueba = nuevoId;
@@ -105,9 +107,8 @@ function registrarSolicitud(){
         /*dataForm.append('id_prueba', id_prueba);*/
    /* });
 */
-
+/*
     console.log("El nuevo ID es:", id_prueba);
-    dataForm.append('id_prueba', id_prueba.toString());
     // Aquí puedes usar el nuevo ID obtenido
     console.log("../../dao/requestRegister.php/?tipoPrueba="+tipoPrueba.value+"&norma="+norma.value+"&normaFile="+normaFile.value+"&especificaciones="+especificaciones.value+"&numParte="+numParte.value+"&descMaterial="+descMaterial.value+"&cdadMaterial="+cdadMaterial+"&fechaSolicitud="+fechaFormateada+"&id_prueba="+id_prueba);
 
@@ -130,4 +131,71 @@ function registrarSolicitud(){
              console.log(err);
          });
 
+}
+*/
+
+async function registrarSolicitud() {
+    try {
+        var id_prueba = await obtenerNuevoId(); // Esperar a que se resuelva la promesa y obtener el nuevo ID
+
+        const dataForm = new FormData();
+
+        var tipoPrueba         = id("tipoPrueba");
+        var norma              = id("norma");
+        var normaFile          = id("normaFile");
+        var tipoPruebaEspecial = id("tipoPruebaEspecial");
+        var otroPrueba         = id("otroPrueba");
+        var numPiezas          = id("numPiezas");
+        var especificaciones   = id ("especificaciones");
+
+        // Para agregar material por número de parte
+        var numParte           = id('numParte');
+        var descMaterial       = id('descMaterial');
+        var cdadMaterial       = id('cdadMaterial');
+
+        var fechaSolicitud= new Date();
+        var fechaFormateada = fechaSolicitud.getFullYear() + '-' + (fechaSolicitud.getMonth() + 1) + '-' + fechaSolicitud.getDate();
+
+
+        dataForm.append('tipoPrueba', tipoPrueba.value.trim());
+        dataForm.append('norma', norma.value.trim());
+        dataForm.append('normaFile', normaFile.value.trim());
+        //dataForm.append('tipoPruebaEspecial', tipoPruebaEspecial.value.trim());
+        //dataForm.append('otroPrueba', otroPrueba.value.trim());
+        //dataForm.append('numPiezas', numPiezas.value.trim());
+        dataForm.append('especificaciones', especificaciones.value.trim());
+        dataForm.append('numParte', numParte.value.trim());
+        dataForm.append('descMaterial', descMaterial.value.trim());
+        dataForm.append('cdadMaterial', cdadMaterial.value.trim());
+        dataForm.append('fechaSolicitud', fechaFormateada);
+        dataForm.append('id_prueba', id_prueba);
+
+
+        console.log("../../dao/requestRegister.php/?tipoPrueba="+tipoPrueba.value+"&norma="+norma.value+"&normaFile="+normaFile.value+"&especificaciones="+especificaciones.value+"&numParte="+numParte.value+"&descMaterial="+descMaterial.value+"&cdadMaterial="+cdadMaterial+"&fechaSolicitud="+fechaFormateada+"&id_prueba="+id_prueba);
+
+
+        fetch('../../dao/requestRegister.php', {
+            method: 'POST',
+            body: dataForm
+        })
+            .then(function (response) {
+                if (response.ok) { //respuesta
+                    window.location.href = "https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/Index.php";
+                } else {
+                    throw "Error en la llamada Ajax";
+                }
+            })
+            .then(function (texto) {
+                console.log(texto);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+
+        console.log("registrarSolicitud-Nuevo ID:", id_prueba);
+        // Resto del código aquí
+    } catch (error) {
+        console.error("Error al registrar la solicitud:", error);
+    }
 }
