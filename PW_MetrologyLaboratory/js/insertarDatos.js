d6366f0177d929f0c6e1450a8787513bb7d2af2b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-19 08:25:45 -0600

    Prueba id_prueba en registrarSolicitud()

--- PW_MetrologyLaboratory/js/insertarDatos.js
+++ PW_MetrologyLaboratory/js/insertarDatos.js
@@ -73,7 +73,12 @@ function registrarSolicitud(){
     var fechaSolicitud= new Date();
     var fechaFormateada = fechaSolicitud.getFullYear() + '-' + (fechaSolicitud.getMonth() + 1) + '-' + fechaSolicitud.getDate();
 
-
+    var id_prueba;
+    idPrueba().then(function(nuevoId) {
+        id_prueba = nuevoId;
+        console.log("El nuevo ID es:", id_prueba);
+        /*dataForm.append('id_prueba', id_prueba);*/
+    });
 
     dataForm.append('tipoPrueba', tipoPrueba.value.trim());
     dataForm.append('norma', norma.value.trim());
@@ -87,12 +92,7 @@ function registrarSolicitud(){
     dataForm.append('cdadMaterial', cdadMaterial.value.trim());
     dataForm.append('fechaSolicitud', fechaFormateada);
 
-    var id_prueba;
-    idPrueba().then(function(nuevoId) {
-        id_prueba = nuevoId;
-        console.log("El nuevo ID es:", id_prueba);
-        /*dataForm.append('id_prueba', id_prueba);*/
-    });
+
 
     /* console.log("../../dao/requestRegister.php/?tipoPrueba="+tipoPrueba.value+"&norma="+norma.value+"&normaFile="+normaFile.value+"&especificaciones="+especificaciones.value+"&numParte="+numParte.value+"&descMaterial="+descMaterial.value+"&cdadMaterial="+cdadMaterial+"&fechaSolicitud="+fechaFormateada+"&id_prueba="+id_prueba);
 
