59687abaf367facd31bb2e80cdf14223decdbe7f
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-21 11:02:44 -0600

    Correccion validacion password

--- PW_MetrologyLaboratory/dao/daoUsuario.php
+++ PW_MetrologyLaboratory/dao/daoUsuario.php
@@ -2,11 +2,6 @@
 
 include_once('connection.php');
 
-/*$Nomina=$_GET['numNomina'];
-$Password=$_GET['password'];
-Usuario($Nomina, $Password);*/
-
-
 function Usuario($Nomina){
     $con = new LocalConector();
     $conexion=$con->conectar();
@@ -16,33 +11,6 @@ function Usuario($Nomina){
 
     mysqli_close($conexion);
 
-   /*
-        $num = $rsconsPro->num_rows;
-
-        if(mysqli_num_rows($rsconsPro) > 0){
-        $row = mysqli_fetch_assoc($rsconsPro);
-        $rows = $rsconsPro ->fetch_assoc();
-        $password_bd = $rows ['passwordHash'];
-        $pass_c = sha1($Password);
-        echo($password_bd == $pass_c);
-
-        if($password_bd == $pass_c){
-
-            return array(
-                'success' => true, // Indicador de éxito
-                'tipoUsuario' => $row['id_tipoUsuario']
-            );
-
-
-
-   if(mysqli_num_rows($rsconsPro) == 1){
-        $row = mysqli_fetch_assoc($rsconsPro);
-        return array(
-            'success' => true, // Indicador de éxito
-            'tipoUsuario' => $row['id_tipoUsuario']
-        );
-    }
-        }*/
     if(mysqli_num_rows($rsconsPro) == 1){
         $row = mysqli_fetch_assoc($rsconsPro);
         return array(
--- PW_MetrologyLaboratory/dao/validarAdministrador.php
+++ PW_MetrologyLaboratory/dao/validarAdministrador.php
@@ -14,15 +14,11 @@ if (!isset($_SESSION["numNomina"]) || !isset($_SESSION["password"])) {
     if($resultado['success']){
         $tipoUsuario = $resultado['tipoUsuario'];
         $password_bd = $resultado['password_bd'];
-        echo($password_bd);
-        echo('pasword Sha1:');
         $passwordS = sha1($Password);
-        echo($passwordS);
-        echo('Resultado sucess, sigue comparar contraseñas');
 
         if($password_bd == $passwordS){
             if($tipoUsuario == 1){
-                // No hacemos ninguna redirección para el tipo de usuario 1
+                header("Location: ../index.php");
                 echo "<script>alert('Acceso correcto')</script>";
             } elseif ($tipoUsuario == 2){
                 header("Location: ../modules/requests/requestsIndex.php");
