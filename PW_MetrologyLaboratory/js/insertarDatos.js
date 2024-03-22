cf3b87a6876e496aa763422d3fb7c095c62b3330
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-22 08:14:41 -0600

    Prueba RegistrarSolicitud

--- PW_MetrologyLaboratory/js/insertarDatos.js
+++ PW_MetrologyLaboratory/js/insertarDatos.js
@@ -109,8 +109,8 @@ async function registrarSolicitud() {
         })
             .then(function (response) {
                 if (response.ok) { //respuesta
-                    alert("Solicitud realizada exitosamente");
                     window.location.href = "../modules/requests/requestsIndex.php";
+                    alert("Solicitud realizada exitosamente");
                 } else {
                     throw "Error en la llamada Ajax";
                 }
