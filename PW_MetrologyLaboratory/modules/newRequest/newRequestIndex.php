5dbe9ffac9d20378c8b87292b8ff509a5749f610
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-22 14:47:35 -0600

    Errores php

--- PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
+++ PW_MetrologyLaboratory/modules/newRequest/newRequestIndex.php
@@ -22,13 +22,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <?php
 
-    ini_set(‘display_errors’, 1);
+    ini_set('display_errors', 1);
 
-    ini_set(‘display_startup_errors’, 1);
+    ini_set('display_startup_errors', 1);
 
     error_reporting(E_ALL);
 
     ?>
+
     <?php
         session_start();
         $nombreUser = $_SESSION['nombreUsuario'];
