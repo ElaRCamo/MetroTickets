c64e6737296707ce98697510a6cd27893ebd900b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-03-27 10:18:08 -0600

    validarSesion

--- PW_MetrologyLaboratory/dao/daoSesionIniciada.php
+++ PW_MetrologyLaboratory/dao/daoSesionIniciada.php
@@ -1,6 +1,9 @@
 <?php
-    if (!isset($_SESSION['tipoUsuario'])) {
-    header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/indexSesion.php");
-    exit;
-    }
+session_start();
+
+// Verificar si la sesión está iniciada
+$sesionIniciada = isset($_SESSION['tipoUsuario']);
+
+// Devolver la respuesta como un objeto JSON
+echo json_encode(array("sesionIniciada" => $sesionIniciada));
 ?>
