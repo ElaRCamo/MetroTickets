713d2f4385c1a46d6ebb84cb7f3bbec62065a7ed
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-12 08:47:13 -0600

    Cambio imagen login

--- PW_MetrologyLaboratory/modules/review/contentReview.php
+++ PW_MetrologyLaboratory/modules/review/contentReview.php
@@ -2,7 +2,7 @@
     <div class="page-header row headerLogo">
         <div class="col divTitle">
             <h1>Resumen de Solicitud 2024-00223</h1>
-            <small>Desgargar pdf</small>
+            <!--<small>Descargar resumen en pdf</small>-->
         </div>
         <div class="logoRight col-sm-3">
             <div>
@@ -14,5 +14,43 @@
         </div>
     </div>
 
+    <div class="container-fluid" id="containerPruebaR" >
+        <div class="row">
+            <div class="col p-3 ">
+                <!--1 of 2-->
+                <p><strong>No. de solicitud:       </strong><span id="numeroPruebaR"></span></p>
+                <p><strong>Fecha de Solicitud:     </strong><span id="fechaSolicitudR"></span></p>
+                <p><strong>Fecha de Respuesta:     </strong><span id="fechaRespuestaR"></span></p>
+                <p><strong>Solicitante:            </strong><span id="solicitanteR"></span></p>
+                <p><strong>Metrólogo:              </strong><span id="metrologoR"></span></p>
+                <p><strong>Tipo de Prueba:         </strong><span id="tipoPruebaSolicitudR"></span></p>
+            </div>
+            <div class="col p-3">
+                <!--2 of 2-->
+                <p><strong>Norma:                  </strong><span id="normaNombreR"></span></p>
+                <p><strong>Documento de la norma:  </strong><span id="archivoNormaR"></span></p>
+                <p><strong>Observaciones:          </strong><span id="observacionesSolicitudR"></span></p>
+                <p><strong>Estatus de la solicitud:</strong><span id="estatusSolicitudR"></p>
+                <p><strong>Prioridad:              </strong><span id="prioridadR"></p><br>
+            </div>
+            <div class="">
+                <div class="">
+                    <h3>Material para medición</h3>
+                    <table class="table table-striped" id="materialesResumen">
+                        <thead>
+                            <tr>
+                                <th>No. de Parte</th>
+                                <th>Material</th>
+                                <th>Cantidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class=""></div>
+            </div>
+        </div>
 </main>
