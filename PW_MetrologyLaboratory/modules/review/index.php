304556e65a116d4c9f42fb1294778acdf96933fb
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-15 11:08:41 -0600

    modificaciones a resumenPrueba(ID_PRUEBA)

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -15,6 +15,10 @@
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/style.css">
+    <!-- -Archivos de jQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <?php
         session_start();
             $nombreUser = $_SESSION['nombreUsuario'];
