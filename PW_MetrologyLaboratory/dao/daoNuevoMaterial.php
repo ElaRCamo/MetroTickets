f3490959ee0761b83b07e105bc9b8694f013021b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-17 14:59:03 -0600

    Nuevo material

--- PW_MetrologyLaboratory/dao/daoNuevoMaterial.php
+++ PW_MetrologyLaboratory/dao/daoNuevoMaterial.php
@@ -6,7 +6,7 @@ if(isset($_POST['descMaterialN'],$_POST['numParteN'],$_POST['imgMaterialN'],$_PO
     $numParte = $_POST['numParteN'];
     $idPlataforma = $_POST['descMPlataformaN'];
 
-    //Se guarda ruta de la imagen
+    /*//Se guarda ruta de la imagen
     $imgMaterial    = $_POST['imgMaterialN'];
     $target_dir     = "https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/imgs/materials/";
     //Quitar espacios del nombre del archivo:
@@ -25,7 +25,7 @@ if(isset($_POST['descMaterialN'],$_POST['numParteN'],$_POST['imgMaterialN'],$_PO
         } else {
             echo "Hubo un error al subir el archivo.";
         }
-    }
+
 
     // Llamar a la función
     if(nuevoMaterial($descMaterial,$numParte,$imgMaterial,$idPlataforma)) {
@@ -33,6 +33,40 @@ if(isset($_POST['descMaterialN'],$_POST['numParteN'],$_POST['imgMaterialN'],$_PO
     } else {
         echo '<script>alert("Error al registrar el material")</script>';
     }
+    }*/
+
+    // Validar el tipo y tamaño del archivo
+    $allowed_types = array('jpg', 'jpeg', 'png', 'gif');
+    $max_size = 10 * 1024 * 1024; // 10MB
+
+    $nombreArchivo = $_FILES["imgMaterialN"]["name"];
+    $file_type = pathinfo($nombreArchivo, PATHINFO_EXTENSION);
+    $file_size = $_FILES['imgMaterialN']['size'];
+
+    if (!in_array(strtolower($file_type), $allowed_types)) {
+        echo '<script>alert("Error: Solo se permiten archivos JPG, JPEG, PNG o GIF.")</script>';
+    } elseif ($file_size > $max_size) {
+        echo '<script>alert("Error: El tamaño del archivo excede el límite permitido de 10MB.")</script>';
+    } else {
+        // Procede con la subida del archivo
+        $target_dir = "https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/imgs/materials/";
+        $imgFileName = $numParte . "-" . str_replace(' ', '-', $nombreArchivo);
+        $imgFileName = str_replace(' ', '-', $imgFileName);
+        $img = $target_dir . $imgFileName;
+        $moverNormaFile = "../imgs/materials/" . $imgFileName;
+
+        if (move_uploaded_file($_FILES["imgMaterialN"]["tmp_name"], $moverNormaFile)) {
+            // Llamar a la función solo si la subida del archivo es exitosa
+            if(nuevoMaterial($descMaterial, $numParte, $img, $idPlataforma)) {
+                echo '<script>alert("Material registrado exitosamente")</script>';
+            } else {
+                echo '<script>alert("Error al registrar el material")</script>';
+            }
+        } else {
+            echo '<script>alert("Hubo un error al subir el archivo.")</script>';
+        }
+    }
+
 } else {
     echo '<script>alert("Error: Faltan datos en el formulario")</script>';
 }
