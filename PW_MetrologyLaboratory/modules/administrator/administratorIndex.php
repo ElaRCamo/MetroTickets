69c2b96e23bad94d9761fec18ce64cddd4ba04c2
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-17 12:27:37 -0600

    Se agregan ventanas modales para los nuevos registros

--- PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
+++ PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
@@ -16,6 +16,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/css/style.css">
     <link rel="stylesheet" href="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/css/administrator.css">
+    <!-- -Archivos de jQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <?php
         session_start();
         $nombreUser = $_SESSION['nombreUsuario'];
