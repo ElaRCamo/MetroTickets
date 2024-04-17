58fa65cd4201768959de90aa3df5600a2c074edc
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-17 12:15:27 -0600

    Se agregan ventanas modales para los nuevos registros

--- PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
+++ PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
@@ -47,6 +47,7 @@
     ?>
 
     <script src="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/js/general.js"></script>
+    <script src="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/js/cargarDatos.js"></script>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
