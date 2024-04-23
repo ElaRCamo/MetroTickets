d129d57850b456acb2127907273bf78031cf3ab6
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-23 14:40:29 -0600

    Se agrega correo para notificar actualizacion

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -85,6 +85,7 @@
 </script>
 <script src="../../js/cargarDatos.js"></script>
 <script src="../../js/actualizarDatos.js"></script>
+<script src="../../js/insertarDatos.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/js/general.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
