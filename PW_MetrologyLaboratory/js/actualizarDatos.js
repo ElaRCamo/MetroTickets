6a1344e3584d1eae58e55b6b3463f386ce338cfe
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-24 07:59:06 -0600

    Correccion en Actualizar Prueba

--- PW_MetrologyLaboratory/js/actualizarDatos.js
+++ PW_MetrologyLaboratory/js/actualizarDatos.js
@@ -16,7 +16,7 @@ function  updatePruebaAdmin(id_review, id_user){
     data.append('resultadosAdmin', resultadosAdmin.value.trim());
     data.append('fechaUpdate', fechaFormateada);
     data.append('id_user', id_user);
-    //alert("estatusPruebaAdmin: "+estatusPruebaAdmin.value.trim() +", prioridadPruebaAdmin: "+prioridadPruebaAdmin.value.trim()+", metrologoAdmin: "+metrologoAdmin.value.trim()+", observacionesAdmin  "+observacionesAdmin.value.trim()+", resultadosAdmin : "+resultadosAdmin.value.trim()+", fechaUpdate "+ fechaFormateada);
+    alert("estatusPruebaAdmin: "+estatusPruebaAdmin.value.trim() +", prioridadPruebaAdmin: "+prioridadPruebaAdmin.value.trim()+", metrologoAdmin: "+metrologoAdmin.value.trim()+", observacionesAdmin  "+observacionesAdmin.value.trim()+", resultadosAdmin : "+resultadosAdmin.value.trim()+", fechaUpdate "+ fechaFormateada);
 
     const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
