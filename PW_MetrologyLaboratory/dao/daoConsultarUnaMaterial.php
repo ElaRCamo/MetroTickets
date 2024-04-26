f7c3cc46add47d5dad6c5e7e504b5f9a970abd98
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-04-26 14:32:53 -0600

    Plataforma: se cargan datos en modal

--- PW_MetrologyLaboratory/dao/daoConsultarUnaMaterial.php
+++ PW_MetrologyLaboratory/dao/daoConsultarUnaMaterial.php
@@ -10,7 +10,7 @@ function seleccionarMaterial($id_material){
 
     $datos = mysqli_query($conex, "SELECT P.id_plataforma, P.descripcionPlataforma, M.id_descripcion, M.descripcionMaterial, M.numeroDeParte, M.imgMaterial 
                                            FROM Plataforma P 
-                                      LEFT JOIN DescripcionMaterial M ON P.id_plataforma = M.id_descripcion 
+                                      LEFT JOIN DescripcionMaterial M ON P.id_plataforma = M.id_plataforma 
                                             AND M.id_descripcion = $id_material 
                                           WHERE P.estatus = 1 
                                           ORDER BY (M.id_descripcion = $id_material) DESC;");
