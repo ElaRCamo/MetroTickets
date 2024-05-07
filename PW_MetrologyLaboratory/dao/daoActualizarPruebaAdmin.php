47cd800b3ed35480216ada3ef220d614b048668d
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-07 11:12:41 -0600

    Se agrega texto al enlace del pdf de la norma

--- PW_MetrologyLaboratory/dao/daoActualizarPruebaAdmin.php
+++ PW_MetrologyLaboratory/dao/daoActualizarPruebaAdmin.php
@@ -27,7 +27,7 @@ function actualizarPrueba($id_prueba,$id_estatus,$id_prioridad, $id_metrologo, $
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    if($resultados !== "") {
+    if($resultados !== "" || $observaciones !== "") {
         $stmt = $conex->prepare("UPDATE Prueba
                                       SET id_estatusPrueba = ?, id_prioridad = ?, id_metrologo = ?, especificacionesLab = ?, rutaResultados = ?, fechaActualizacion = ?, fechaRespuesta = ?, id_administrador = ?
                                     WHERE id_prueba = ?");
