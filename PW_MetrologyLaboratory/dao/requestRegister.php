54dd5b19f269dcf60c477bb3969aa2086c2dc612
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-13 09:22:51 -0600

    Prueba newRequest

--- PW_MetrologyLaboratory/dao/requestRegister.php
+++ PW_MetrologyLaboratory/dao/requestRegister.php
@@ -3,57 +3,51 @@ include_once('connection.php');
 session_start();
 
 // Verificar si los datos están presentes y asignarlos de manera segura
-if(isset($_POST['tipoPrueba'], $_POST['norma'], $_SESSION['nomina'], $_POST['especificaciones'])) {
+if(isset($_POST['tipoPrueba'], $_SESSION['nomina'], $_POST['especificaciones'], $_POST['materiales'], $_POST['cantidades'], $_POST['fechaSolicitud'], $_POST['id_prueba'])) {
+    // Asignar variables
     $tipoPrueba     = $_POST['tipoPrueba'];
-    $id_prueba      = '2024-0199';
-    //, $_POST['materiales'], $_POST['cantidades'], $_POST['id_prueba']
+    $id_prueba      = $_POST['id_prueba'];
 
-    if($tipoPrueba == 3 || $tipoPrueba == 4 || $tipoPrueba == 5){ //si se requiere norma por tipo de prueba
-        $norma          = $_POST['norma'];
-        //guardar los archivos de la norma
-        $target_dir     = "https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/archivos/";
-        //Quitar espacios del nombre del archivo:
+    // Verificar si se requiere norma por tipo de prueba
+    if($tipoPrueba == 3 || $tipoPrueba == 4 || $tipoPrueba == 5) {
+        $norma = $_POST['norma'];
+        // Guardar los archivos de la norma
+        $target_dir     = "../archivos/"; // Ruta del directorio de destino para guardar archivos
         $nombreArchivo  = $_FILES["normaFile"]["name"];
         $normaFileName  = $id_prueba . "-" . str_replace(' ', '-', $nombreArchivo);
         $normaFile      = $target_dir . $normaFileName;
-        $moverNormaFile = "../archivos/" . $normaFileName;
+        $moverNormaFile = $target_dir . $normaFileName;
 
         if ($_FILES["normaFile"]["error"] > 0) {
             echo json_encode(array("error" => true, 'message' => "Error: " . $_FILES["normaFile"]["error"] ));
-            //$response = array( "error" => "Error: " . $_FILES["normaFile"]["error"] );
         } else {
-            // mover el archivo cargado a la ubicación deseada
+            // Procesar el archivo de norma si no hay errores
             move_uploaded_file($_FILES["normaFile"]["tmp_name"], $moverNormaFile);
-                //$response = array("message" => "El archivo " . htmlspecialchars($normaFileName) . " ha sido subido correctamente.");
-            /*else {
-                echo json_encode(array("error" => "Hubo un error al subir el archivo."));
-                //$response = array("error" => "Hubo un error al subir el archivo.");
-            }*/
         }
-    }else{ //El tipo de prueba no requiere especificar norma
+    } else {
+        // El tipo de prueba no requiere especificar norma
         $norma     = 'No aplica';
         $normaFile = 'No aplica';
     }
 
+    // Asignar otras variables
     $idUsuario            = $_SESSION['nomina'];
     $tipoPruebaEspecial   = ($_POST['tipoPrueba'] != 5) ?  5 : $_POST['tipoPruebaEspecial'] ;
     $otroPrueba           = ($tipoPruebaEspecial  != 4) ? 'No aplica' : $_POST['otroPrueba'] ;
     $especificaciones     = $_POST['especificaciones'];
-    $fechaSolicitud       =  date('Y-m-d_H-i-s');
+    $fechaSolicitud       = $_POST['fechaSolicitud'];
 
-    // materiales y cantidades son strings separadas por comas, asi que se convierten en arrays usando explode
-    //$descMateriales = explode(', ', $_POST['materiales']);
-    //$cdadMateriales = explode(', ', $_POST['cantidades']);
-    $descMateriales = explode(', ', $_POST['material[]']);
-    $cdadMateriales = explode(', ', $_POST['cdadesMaterial[]']);
+    // Convertir materiales y cantidades en arrays
+    $descMateriales = explode(', ', $_POST['materiales']);
+    $cdadMateriales = explode(', ', $_POST['cantidades']);
 
-    RegistrarSolicitud($tipoPrueba, $norma, $normaFile, $idUsuario,$tipoPruebaEspecial, $otroPrueba, $especificaciones, $descMateriales, $cdadMateriales, $fechaSolicitud, $id_prueba);
-
+    // Llamar a la función para registrar la solicitud
+    RegistrarSolicitud($tipoPrueba, $norma, $normaFile, $idUsuario, $tipoPruebaEspecial, $otroPrueba, $especificaciones, $descMateriales, $cdadMateriales, $fechaSolicitud, $id_prueba);
 } else {
-    //$response = array("error" => "<script>alert('Error: Faltan datos en el formulario')</script>");
+    // Mostrar mensaje de error si faltan datos en el formulario
     echo json_encode(array("error" => true, 'message' => "<script>alert('Error: Faltan datos en el formulario')</script>"));
-
 }
+
 
 function RegistrarSolicitud($tipoPrueba, $norma, $normaFile, $idUsuario, $tipoPruebaEspecial, $otroPrueba, $especificaciones, $descMateriales, $cdadMateriales, $fechaSolicitud, $id_prueba)
 {
@@ -80,17 +74,21 @@ function RegistrarSolicitud($tipoPrueba, $norma, $normaFile, $idUsuario, $tipoPr
         $insertMaterial->bind_param("sii", $id_prueba, $cdadMaterial, $descMaterial);
         $rInsertMaterial = $rInsertMaterial && $insertMaterial->execute();
     }
+    // Confirmar o hacer rollback de la transacción
     if(!$rInsertSolicitud || !$rInsertMaterial) {
         $conex->rollback();
         $conex->close();
         echo json_encode(array('error' => true, 'message' => 'Error en RegistrarSolicitud'));
         exit;
+        //return false;
     } else {
         $conex->commit();
         $conex->close();
         echo json_encode(array('error' => false, 'message' => 'Datos insertados correctamente'));
         exit;
+        //return true;
     }
+
 }
 
 //bind_param(): Es un método de la clase mysqli_stmt que se utiliza para vincular parámetros a la consulta preparada.
