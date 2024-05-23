d888bd50164dbf495716c2418ddfff510a8c5bd1
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-23 10:17:25 -0600

    Se agrega archivo validaciones.js

--- PW_MetrologyLaboratory/js/cargarDatos.js
+++ PW_MetrologyLaboratory/js/cargarDatos.js
@@ -770,11 +770,12 @@ function agregarMaterial() {
         + '<input id="numParte' + indexMaterial + '" name="numPartes[]" type="text" class="numParteInput" placeholder="Número de parte*" readonly>'
         + '</div>'
         + '</div>'
-        + '<div class="col buttons-container">'
+        + '<div class="col-xl-4 buttons-container">'
         + '<a href="#" class="remove-lnk removeBtn" id="' + indexMaterial + '"><i class="las la-trash-alt"></i></a>'
         + '<a href="#" class="agregarButton" id="addNumParte' + indexMaterial + '"><i class="las la-plus-square"></i></a>'
         + '</div>'
         + '</div>'
+
         + '</div>');
     newRow.appendTo('#contenedorFormulario');
 }
