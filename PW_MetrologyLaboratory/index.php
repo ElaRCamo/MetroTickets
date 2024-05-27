3e19f57b2012f59402b82426467a7289d7dfe979
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-27 12:51:32 -0600

    Se agrega grafico de las pruebas por mes

--- PW_MetrologyLaboratory/index.php
+++ PW_MetrologyLaboratory/index.php
@@ -21,6 +21,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <?php
         session_start();
         $nombreUser = $_SESSION['nombreUsuario'];
