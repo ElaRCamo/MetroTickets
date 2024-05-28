5f1ad66010b534f0363e94c65bb17d592b4cd3e3
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-28 11:21:51 -0600

    Se crea dataTable

--- PW_MetrologyLaboratory/js/cargarDatos.js
+++ PW_MetrologyLaboratory/js/cargarDatos.js
@@ -243,7 +243,8 @@ const dataTableOptions = {
     columnDefs:[
         {className: "centered", targets: [0,1,2,3,4,5,6,7,8]},
         {orderable: false, targets: [8]},
-        {width: "10%", targets: [0]}
+        {width: "8%", targets: [0,1,2,3,4,5,6,7]},
+        {width: "16%", targets: [4]}
     ],
     pageLength:15,
     destroy: true,
