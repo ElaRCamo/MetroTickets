d75fc42a48e58668d4b80435ae2d7be741c36d30
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-28 11:39:06 -0600

    Se crea dataTable

--- PW_MetrologyLaboratory/modules/requests/requestsIndex.php
+++ PW_MetrologyLaboratory/modules/requests/requestsIndex.php
@@ -21,6 +21,8 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- -Archivos de Datatable-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css"/>
+
 
     <?php
         session_start();
@@ -62,9 +64,6 @@
             <?php } ?>
 
         });
-        window.addEventListener("load",async () => {
-            await initDatatable();
-        })
     </script>
     <script src="../../js/cargarDatos.js"></script>
     <script src="https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/js/general.js"></script>
@@ -72,10 +71,19 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-
     <!-- -Archivos de Datatable-->
     <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+
+        /*window.addEventListener("load",async () => {
+            await initDatatable();
+        })*/
+
+        $(document).ready(function (){
+            $(#listadoPruebas).DataTable();
+        });
+    </script>
 </body>
 </html>
 
