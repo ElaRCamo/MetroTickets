28db7e3343fd25c2343d42d4b68aeb7bd02a09e3
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-28 10:33:40 -0600

    Se crea dataTable

--- PW_MetrologyLaboratory/modules/requests/requestsIndex.php
+++ PW_MetrologyLaboratory/modules/requests/requestsIndex.php
@@ -14,13 +14,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;500;700&display=swap" rel="stylesheet">
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../css/style.css">
     <!-- -Archivos de jQuery-->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- -Archivos de Datatable-->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
 
 
     <?php
