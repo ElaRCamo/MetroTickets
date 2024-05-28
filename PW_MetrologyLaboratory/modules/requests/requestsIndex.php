a2aa2d01f55d9cea3ade9488078a03146752cfcf
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-28 11:44:03 -0600

    Se crea dataTable

--- PW_MetrologyLaboratory/modules/requests/requestsIndex.php
+++ PW_MetrologyLaboratory/modules/requests/requestsIndex.php
@@ -81,7 +81,7 @@
         })*/
 
         $(document).ready(function (){
-            $(#listadoPruebas).DataTable();
+            $('#listadoPruebas').DataTable();
         });
     </script>
 </body>
