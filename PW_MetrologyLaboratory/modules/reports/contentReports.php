60edaf316e5c4372231ebeb0eeca1ccf833bb012
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-29 13:14:07 -0600

    Se agrega tabla para reportes

--- PW_MetrologyLaboratory/modules/reports/contentReports.php
+++ PW_MetrologyLaboratory/modules/reports/contentReports.php
@@ -6,46 +6,44 @@
 
     <div class="page-content">
         <div class="container" id="divFormReporte">
-            <form id="reporteMensual" method="POST" class="popup-form">
-                <div class="container">
-                    <table class="table-responsive">
-                        <thead>
-                            <tr>
-                                <th>
-                                    <label for="tipoReporte">Tipo de reporte</label>
-                                </th>
-                                <th>
-                                    <label for="anio">Año</label>
-                                </th>
-                                <th>
-                                    <label for="mes">Mes</label>
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <select class="form-control" id="tipoReporte" name="tipoReporte" required data-error="Por favor seleccione un tipo de reporte válido.">
-                                        <option value="">Seleccione el tipo de reporte*</option>
-                                        <option value="1">Sólo estadísticas</option>
-                                        <option value="2">Detallado</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <select class="form-control" id="anio" name="anio" required data-error="Por favor seleccione un año válido.">
-                                        <option value="">Seleccione el año*</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <select class="form-control" id="mes" name="mes" required data-error="Por favor seleccione un mes válido.">
-                                        <option value="">Seleccione el mes*</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="container">
+            <form id="reporteMensual" method="POST" class="popup-form wrapper">
+                <div class="container row">
+                    <div class="col-sm-4" id="divTipoReporte">
+                        <label for="tipoReporte">Tipo de reporte</label>
+                        <div class="form-group" id="divTipoReporte">
+                            <select class="form-control" id="tipoReporte" name="tipoReporte" required data-error="Por favor seleccione un tipo de reporte válido.">
+                                <option value="">Seleccione el tipo de reporte*</option>
+                                <option value="1">Sólo estadísticas</option>
+                                <option value="2">Detallado</option>
+                            </select>
+                            <div class="input-group-icon"><i class="las la-clipboard-check"></i></div>
+                            <div class="invalid-feedback"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4" id="divAnioReporte">
+                        <label for="anio">Año</label>
+                        <div class="form-group" id="divAnioReporte">
+                            <select class="form-control" id="anio" name="anio" required data-error="Por favor seleccione un año válido.">
+                                <option value="">Seleccione el año*</option>
+                            </select>
+                            <div class="input-group-icon"><i class="las la-calendar-check"></i></div>
+                            <div class="invalid-feedback"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4" id="divMesReporte">
+                        <label for="mes">Mes</label>
+                        <div class="form-group" id="divMesReporte">
+                            <select class="form-control" id="mes" name="mes" required data-error="Por favor seleccione un mes válido.">
+                                <option value="">Seleccione el mes*</option>
+                            </select>
+                            <div class="input-group-icon"><i class="las la-calendar-week"></i></div>
+                            <div class="invalid-feedback"></div>
+                        </div>
+                    </div>
+
+                <div class="container" id="divBtnReporte">
                     <button type="submit" onclick="" class="btn btn-primary" id="btnReporte"><i class="las la-newspaper"></i>Generar reporte</button>
                 </div>
             </form>
