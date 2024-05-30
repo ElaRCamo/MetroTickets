1250a06738a0003b4705928324d119e424566986
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-30 09:06:24 -0600

    Se agrega tabla para reportes

--- PW_MetrologyLaboratory/dao/daoAnioMes.php
+++ PW_MetrologyLaboratory/dao/daoAnioMes.php
@@ -3,20 +3,16 @@
 include_once('connection.php');
 
 $anio = $_GET['anio'];
-ContadorMes();
+ContadorMes($anio);
 
-function ContadorMes(){
+function ContadorMes($anio){
     $con = new LocalConector();
     $conex = $con->conectar();
 
     $datos = mysqli_query($conex, "SELECT DISTINCT MONTH(fechaRespuesta) AS mes
                                            FROM Prueba
-                                          WHERE YEAR(fechaRespuesta) = 2023 AND fechaRespuesta IS NOT NULL AND fechaRespuesta != '0000-00-00'
-                                          UNION
-                                         SELECT DISTINCT MONTH(fechaSolicitud) AS mes
-                                           FROM Prueba
-                                          WHERE YEAR(fechaSolicitud) = 2023 AND fechaSolicitud IS NOT NULL AND fechaSolicitud != '0000-00-00'
-                                          ORDER BY mes;");
+                                          WHERE YEAR(fechaRespuesta) = $anio AND fechaRespuesta IS NOT NULL 
+                                            AND fechaRespuesta != '0000-00-00'");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
