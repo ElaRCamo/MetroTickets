9dfd51bf97cced3589bc5683ddf640ef99a74103
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-04 10:29:07 -0600

    Correccion function registrarUsuario()

--- PW_MetrologyLaboratory/dao/userRegister.php
+++ PW_MetrologyLaboratory/dao/userRegister.php
@@ -1,7 +1,9 @@
 <?php
-header('Content-Type: application/json');
+
 include_once('connection.php');
-require 'daoUsuario.php';
+include_once( 'daoUsuario.php');
+
+header('Content-Type: application/json');
 
 if(isset($_POST['nombreUsuario'], $_POST['correo'], $_POST['numNomina'], $_POST['password'])) {
     $nombreUsuario = $_POST['nombreUsuario'];
@@ -17,8 +19,8 @@ echo json_encode($response);
 function RegistrarUsuario($nombreUsuario, $correo, $Nomina, $password)
 {
     $passwordS = sha1($password);
-    $Nomina    = str_pad($Nomina, 8, "0", STR_PAD_LEFT);
-    $resultado = Usuario($Nomina);
+    $numNomina    = str_pad($Nomina, 8, "0", STR_PAD_LEFT);
+    $resultado = Usuario($numNomina);
 
     if ($resultado['success']) {
         return array('status' => 'error', 'message' => 'El usuario ya existe, verifique sus datos');
@@ -27,12 +29,14 @@ function RegistrarUsuario($nombreUsuario, $correo, $Nomina, $password)
         $conex = $con->conectar();
 
         $insertUsuario = "INSERT INTO `Usuario` (`id_usuario`, `nombreUsuario`, `correoElectronico`, `passwordHash`) 
-                               VALUES ('$Nomina', '$nombreUsuario', '$correo', '$passwordS')";
-        $rInsertUsuario = mysqli_query($conex, $insertUsuario);
+                               VALUES (?,?,?,?)";
+        $insertUsuario->bind_param("ssss", $numNomina,$nombreUsuario,$correo,$passwordS);
+        $resultado = $insertUsuario->execute();
 
-        mysqli_close($conex);
+        // Cerrar la conexión
+        $conex->close();
 
-        if (!$rInsertUsuario) {
+        if(!$resultado) {
             return array('status' => 'error', 'message' => 'Error al registrar el usuario');
         } else {
             return array('status' => 'success', 'message' => 'Usuario registrado exitosamente');
