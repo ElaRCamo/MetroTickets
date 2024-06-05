01fe030eda1aa6787abc00504fdff7040242e4ce
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-05 11:32:36 -0600

    Se agrega correo de recuperación de contraseña

--- PW_MetrologyLaboratory/Mailer/MailerRecuperarPassword.php
+++ PW_MetrologyLaboratory/Mailer/MailerRecuperarPassword.php
@@ -74,7 +74,7 @@ function emailRecuperarPassword($destinatario, $asunto, $mensaje)
     $mail = new PHPMailer(true);
 
     try {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->SMTPDebug = 0;
         $mail->isSMTP();
         $mail->Host = 'smtp.hostinger.com';
         $mail->Port = 465;
