6b0378892eca4c49d24cc72e5a5a927d667d7c40
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-13 12:52:05 -0600

    correccion daoActualizarMaterial

--- PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
+++ PW_MetrologyLaboratory/modules/administrator/administratorIndex.php
@@ -20,9 +20,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
     <!-- DataTable -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" />
-
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.js" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/js/responsive.bootstrap5.js" />
     <!-- Custom CSS-->
     <link rel="stylesheet" href="../../css/style.css"/>
     <link rel="stylesheet" href="../../css/administrator.css">
@@ -78,5 +75,7 @@
     <!-- DataTable -->
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.bootstrap5.js"></script>
 </body>
 </html>
