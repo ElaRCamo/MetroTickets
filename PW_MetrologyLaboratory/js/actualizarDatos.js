d68f7248a6828f0a717959b4cd10060c4d99f88e
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-20 13:31:14 -0600

    Se agrega logica para guardar resultados

--- PW_MetrologyLaboratory/js/actualizarDatos.js
+++ PW_MetrologyLaboratory/js/actualizarDatos.js
@@ -127,7 +127,7 @@ function  updatePruebaAdmin(id_review, id_user){
     data.append('observacionesAdmin', observacionesAdmin.value.trim());
     data.append('fechaUpdate', fechaFormateada);
     data.append('id_user', id_user);
-    alert("estatusPruebaAdmin: "+estatusPruebaAdmin.value.trim() +", prioridadPruebaAdmin: "+prioridadPruebaAdmin.value.trim()+", metrologoAdmin: "+metrologoAdmin.value.trim()+", observacionesAdmin  "+observacionesAdmin.value.trim()+", resultadosAdmin : "+resultadosAdmin.value.trim()+", fechaUpdate "+ fechaFormateada);
+    alert("estatusPruebaAdmin: "+estatusPruebaAdmin.value.trim() +", prioridadPruebaAdmin: "+prioridadPruebaAdmin.value.trim()+", metrologoAdmin: "+metrologoAdmin.value.trim()+", observacionesAdmin  "+observacionesAdmin.value.trim()+", resultadosAdmin : "+resultados+", fechaUpdate "+ fechaFormateada);
 
 
     const swalWithBootstrapButtons = Swal.mixin({
