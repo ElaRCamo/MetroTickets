eae7ef28016f95d025e1cff4a12d0372e1e9db12
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-24 09:46:36 -0600

    Se agrega  fechaCompromiso en daoActua

--- PW_MetrologyLaboratory/js/actualizarDatos.js
+++ PW_MetrologyLaboratory/js/actualizarDatos.js
@@ -113,7 +113,7 @@ function  updatePruebaAdmin(id_review, id_user){
     data.append('id_user', id_user);
 
     if(estatusPruebaAdmin.value==='2'){
-        data.append('fechaCompromiso', fechaCompromiso);
+        data.append('fechaCompromiso', fechaCompromiso.value.trim());
     }
     alert("fechaCompromiso " + fechaCompromiso.value.trim()+"estatusPruebaAdmin: "+estatusPruebaAdmin.value.trim() +", prioridadPruebaAdmin: "+prioridadPruebaAdmin.value.trim()+", metrologoAdmin: "+metrologoAdmin.value.trim()+", observacionesAdmin  "+observacionesAdmin.value.trim()+", resultadosAdmin : "+resultados+", fechaUpdate "+ fechaFormateada);
 
