19205eb6f5700244b472ab037b76133a2043df9f
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-06-24 08:52:54 -0600

    Se agrega input fechaCompromiso

--- PW_MetrologyLaboratory/js/cargarDatos.js
+++ PW_MetrologyLaboratory/js/cargarDatos.js
@@ -1377,6 +1377,24 @@ function llenarPrioridadPrueba(){
     });
 }
 
+function llenarEstatusPrueba(){
+    $.getJSON('https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/dao/daoEstatusPrueba.php', function (data){
+        var selectS = id("estatusPruebaAdmin");
+        selectS.innerHTML = ""; //limpiar contenido
+
+        for (var j = 0; j < data.data.length; j++) {
+            var createOption = document.createElement("option");
+            createOption.value = data.data[j].id_estatusPrueba;
+            createOption.text = data.data[j].descripcionEstatus;
+            selectS.appendChild(createOption);
+            // Si el valor actual coincide con id_estatusSol, se selecciona por defecto
+            if (data.data[j].id_estatusPrueba === id_estatusSol) {
+                createOption.selected = true;
+            }
+        }
+    });
+}
+
 function consultarMetrologos(){
     $.getJSON('https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/dao/daoMetrologos.php', function (data){
         var selectS = id("metrologoAdmin");
