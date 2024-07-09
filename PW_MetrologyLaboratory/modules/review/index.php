9861ce0f480e2f73af6948b6e30bc0230d1afbab
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-09 09:31:01 -0600

    function resumenPrueba(ID_PRUEBA)

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -187,9 +187,7 @@
     });
 </script>
 <script src="../../js/general.js"></script>
-<script src="../../js/cargarDatos.js"></script>
-<script src="../../js/actualizarDatos.js"></script>
-<script src="../../js/insertarDatos.js"></script>
+<script src="../../js/review.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
