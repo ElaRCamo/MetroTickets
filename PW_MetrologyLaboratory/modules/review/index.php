54bb8e6b45b50f4d3bcb3503d185f919321dd8fb
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-10 15:03:47 -0600

    Se agrega  daoConsultarSolicitante.php

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -20,6 +20,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <?php
+    include_once('../../dao/daoConsultarSolicitante.php');
         //Sacar los datos de la sesión
         session_start();
             $nombreUser = $_SESSION['nombreUsuario'];
@@ -46,45 +47,6 @@
     } else {
         $id_prueba = "No aplica";
         $solicitante = "No aplica";
-    }
-
-    function consultarSolicitante($id_prueba)
-    {
-        $response = array('status' => 'error', 'message' => 'Error desconocido');
-
-        try {
-            $con = new LocalConector();
-            $conex = $con->conectar();
-
-            if ($conex->connect_error) {
-                throw new Exception('Error en la conexión a la base de datos: ' . $conex->connect_error);
-            }
-
-            // Consultando las piezas ya registradas
-            $selectQuery = $conex->prepare("SELECT id_solicitante FROM Pruebas WHERE id_prueba = ?");
-            $selectQuery->bind_param("s", $id_prueba);
-            $selectQuery->execute();
-            $result = $selectQuery->get_result();
-
-            if ($result) {
-                $row = $result->fetch_assoc();
-                if ($row) {
-                    $response = array('status' => 'success', 'id_solicitante' => $row['id_solicitante']);
-                } else {
-                    $response = array('status' => 'error', 'message' => 'No se encontró el solicitante');
-                }
-            } else {
-                $response = array('status' => 'error', 'message' => 'Error al consultar solicitante');
-            }
-
-            // Cerrando la declaración y la conexión
-            $selectQuery->close();
-            $conex->close();
-        } catch (Exception $e) {
-            $response['message'] = $e->getMessage();
-        }
-
-        return $response;
     }
 
     if ($tipoUser == null){
