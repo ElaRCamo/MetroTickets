8776ccbe2f2d44b2d2a8d4625b84b1c933a322d3
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-11 09:25:25 -0600

    Se agrega  daoConsultarSolicitante.php

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -43,7 +43,7 @@
         $solicitante = '00030293';
         echo $solicitante;
 
-        if ($idUsuario !== $solicitante) {
+        if ($idUsuario != $solicitante) {
             header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/requests/requestsIndex.php");
             exit(); // Añadir exit después de header para asegurar que el script se detenga
         }
