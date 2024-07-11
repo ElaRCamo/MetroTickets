a889bfcc563c14ada93174aa09ad590ee4df93d6
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-11 10:42:51 -0600

    Se agrega  daoConsultarSolicitante.php

--- PW_MetrologyLaboratory/dao/daoConsultarSolicitante.php
+++ PW_MetrologyLaboratory/dao/daoConsultarSolicitante.php
@@ -1,6 +1,41 @@
 <?php
 include_once('connection.php');
-header('Content-Type: application/json');
+
+function consultarSolicitante($id_prueba)
+{
+    try {
+        $con = new LocalConector();
+        $conex = $con->conectar();
+
+        if ($conex->connect_error) {
+            throw new Exception('Error en la conexión a la base de datos: ' . $conex->connect_error);
+        }
+
+        // Consultando las piezas ya registradas
+        $selectQuery = $conex->prepare("SELECT id_solicitante FROM Pruebas WHERE id_prueba = ?");
+        $selectQuery->bind_param("s", $id_prueba);
+        $selectQuery->execute();
+        $result = $selectQuery->get_result();
+
+        $id_solicitante = null;
+        if ($result) {
+            $row = $result->fetch_assoc();
+            if ($row) {
+                $id_solicitante = $row['id_solicitante'];
+            }
+        }
+
+        // Cerrando la declaración y la conexión
+        $selectQuery->close();
+        $conex->close();
+    } catch (Exception $e) {
+        // Manejo de errores opcional
+        $id_solicitante = null;
+    }
+
+    return $id_solicitante;
+}
+/*header('Content-Type: application/json');
 
 function consultarSolicitante($id_prueba)
 {
@@ -39,5 +74,5 @@ function consultarSolicitante($id_prueba)
     }
 
     return $response;
-}
+}*/
 ?>
