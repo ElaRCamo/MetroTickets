4d2674d5cadcbc68c77317b6aa5b7f0ab3cef7e6
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-11 10:16:08 -0600

    Se agrega  daoConsultarSolicitante.php

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -1,50 +1,5 @@
 <?php
 
-require_once('../../dao/daoConsultarSolicitante.php');
-
-session_start();
-$nombreUser = $_SESSION['nombreUsuario'];
-$tipoUser = $_SESSION['tipoUsuario'];
-$idUsuario = $_SESSION['nomina'];
-$fotoUsuario = $_SESSION['fotoUsuario'];
-
-echo "tipo user:".$tipoUser;
-$solicitante = "No aplica";
-
-if ($tipoUser == null) {
-    header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/indexSesion.php");
-    exit();
-} else if ($tipoUser == 3) {
-    // Obtener la parte de la consulta de la URL actual
-    $queryString = $_SERVER['QUERY_STRING'];
-
-    // Obtener los parámetros de la consulta en un array asociativo
-    parse_str($queryString, $params);
-
-    // Verificar si existe el parámetro id_prueba y obtener su valor
-    if (isset($params['id_prueba'])) {
-        $id_prueba = $params['id_prueba'];
-        echo $idUsuario;
-        $solicitante = 5;
-
-        $consultaSolicitante = consultarSolicitante($id_prueba);
-        // Verificar y manejar la respuesta de la consulta
-        if ($consultaSolicitante['status'] == 'success') {
-            $solicitante = $consultaSolicitante['id_solicitante'];
-        } else {
-            $solicitante = "No se encontró solicitante";
-        }
-    }
-
-}
-echo " Solicitante: ".$solicitante;
-echo " idUsuario: ".$idUsuario;
-
-if ($idUsuario !== $solicitante) {
-    header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/requests/requestsIndex.php");
-    exit();
-}
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -71,6 +26,48 @@ if ($idUsuario !== $solicitante) {
 </head>
 <body >
 <?php
+require_once('../../dao/daoConsultarSolicitante.php');
+
+session_start();
+$nombreUser = $_SESSION['nombreUsuario'];
+$tipoUser = $_SESSION['tipoUsuario'];
+$idUsuario = $_SESSION['nomina'];
+$fotoUsuario = $_SESSION['fotoUsuario'];
+
+echo "tipo user:".$tipoUser;
+$solicitante = "No aplica";
+
+if ($tipoUser == null) {
+    header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/indexSesion.php");
+    exit();
+} else if ($tipoUser == 3) {
+    // Obtener la parte de la consulta de la URL actual
+    $queryString = $_SERVER['QUERY_STRING'];
+
+    // Obtener los parámetros de la consulta en un array asociativo
+    parse_str($queryString, $params);
+
+    // Verificar si existe el parámetro id_prueba y obtener su valor
+    if (isset($params['id_prueba'])) {
+        $id_prueba = $params['id_prueba'];
+
+        $consultaSolicitante = consultarSolicitante($id_prueba);
+
+        // Verificar y manejar la respuesta de la consulta
+        if ($consultaSolicitante['status'] == 'success') {
+            $solicitante = $consultaSolicitante['id_solicitante'];
+        } else {
+            $solicitante = "No se encontró solicitante";
+        }
+    }
+}
+echo " Solicitante: ".$solicitante;
+echo " idUsuario: ".$idUsuario;
+
+if ($idUsuario !== $solicitante) {
+    header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/requests/requestsIndex.php");
+    exit();
+}
     # Header section
     require_once('../../header.php');
     require_once('../../navbar.php');
@@ -80,7 +77,7 @@ if ($idUsuario !== $solicitante) {
     # Content section
     require_once('../../footer.php');
     # Ventanas modales
-    include 'modalResultados.php';
+    require_once('modalResultados.php');
     ?>
 <script>
     let id_review;
