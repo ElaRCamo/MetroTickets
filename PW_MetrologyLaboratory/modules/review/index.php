bdd3a61fc7f0d5f3d355da33ddd02bb0fc9f2aa1
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-11 10:45:26 -0600

    Se agrega  daoConsultarSolicitante.php

--- PW_MetrologyLaboratory/modules/review/index.php
+++ PW_MetrologyLaboratory/modules/review/index.php
@@ -19,6 +19,47 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php
+    require_once('../../dao/daoConsultarSolicitante.php');
+
+    session_start();
+    $nombreUser = $_SESSION['nombreUsuario'];
+    $tipoUser = $_SESSION['tipoUsuario'];
+    $idUsuario = $_SESSION['nomina'];
+    $fotoUsuario = $_SESSION['fotoUsuario'];
+
+    $solicitante = "No aplica";
+
+    if ($tipoUser == null) {
+        header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/sesion/indexSesion.php");
+        exit();
+    } else if ($tipoUser == 3) {
+        // Obtener la parte de la consulta de la URL actual
+        $queryString = $_SERVER['QUERY_STRING'];
+
+        // Obtener los parámetros de la consulta en un array asociativo
+        parse_str($queryString, $params);
+
+        // Verificar si existe el parámetro id_prueba y obtener su valor
+        if (isset($params['id_prueba'])) {
+            $id_prueba = $params['id_prueba'];
+
+            $consultaSolicitante = consultarSolicitante($id_prueba);
+
+            // Verificar y manejar la respuesta de la consulta
+            if ($consultaSolicitante['status'] == 'success') {
+                $solicitante = $consultaSolicitante['id_solicitante'];
+            } else {
+                $solicitante = "No se encontró solicitante";
+            }
+        }
+    }
+
+    if ($idUsuario !== $solicitante) {
+        header("Location: https://arketipo.mx/Produccion/ML/PW_MetrologyLaboratory/modules/requests/requestsIndex.php");
+        exit();
+    }
+    ?>
 </head>
 <body >
 <?php
@@ -33,8 +74,6 @@
     require_once('../../footer.php');
     # Ventanas modales
     require_once('modalResultados.php');
-# Permisos
-require_once('../../dao/validacionPermisosReview.php');
     ?>
 <script>
     let id_review;
