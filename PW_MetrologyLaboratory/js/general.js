079b268fd1c0fffefc60ffd8666f3f0308c7f3ec
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-17 15:02:47 -0600

    Actualizacion general.js

--- PW_MetrologyLaboratory/js/general.js
+++ PW_MetrologyLaboratory/js/general.js
@@ -142,6 +142,69 @@ function updatePerfilUsuario(){
  * ********************************************/
 
 
+function validarCorreo(id,div){
+    const correoInput = document.getElementById(id);
+    const correo = correoInput.value.trim();
+    const dominioPermitido = '@grammer.com';
+
+    if (!correo.endsWith(dominioPermitido)) {
+        document.getElementById(div).innerHTML="<label id='lError' style='color:red; font-size:0.7rem;text-align: center;'>El correo debe tener el dominio '@grammer.com'</label>";
+        return false;
+    }else{
+        if(document.getElementById('lError')) {
+            document.getElementById('lError').style.display = "none";
+        }
+
+        return true;
+    }
+
+}
+
+function validarPasswords(p1,p2,div){
+    var password1 = document.getElementById(p1).value;
+    var password2 = document.getElementById(p2).value;
+
+    if (password1 !== password2) {
+        document.getElementById(div).innerHTML="<label id='lbError' style='color:red;  text-align: center;'>Las contraseñas no coinciden.</label>";
+        return false;
+    }else{
+        if(document.getElementById('lbError')) {
+            document.getElementById('lbError').style.display = "none";
+        }
+
+        return true;
+    }
+}
+
+function validarFormulario() {
+    let isValid = true;
+
+    // Validar que los campos no estén vacíos
+    let inputs = document.querySelectorAll('#registrarseForm input[required]');
+    inputs.forEach(function(input) {
+        if (!input.value.trim()) {
+            showErrorMessage(input, input.dataset.error);
+            isValid = false;
+        }else if (input.value.trim()) {
+            hideErrorMessage(input);
+        }
+    });
+
+    return isValid;
+}
+
+function showErrorMessage(input, message) {
+    let errorDiv = input.nextElementSibling.nextElementSibling;
+    errorDiv.textContent = message;
+    errorDiv.style.display = 'block';
+}
+
+function hideErrorMessage(input) {
+    let errorDiv = input.nextElementSibling.nextElementSibling;
+    errorDiv.style.display = 'none';
+}
+
+
 // Función para cerrar sesión
 function cerrarSesion() {
     const xhttp = new XMLHttpRequest();
--- PW_MetrologyLaboratory/js/insertarDatos.js
+++ PW_MetrologyLaboratory/js/insertarDatos.js
@@ -96,65 +96,6 @@ function enviarCorreoNuevoUsuario(nombre, id, correo){
         });
 }
 
-function recuperarPassword() {
-    var esCorreoValido = validarCorreo('correoRecuperacion','avisoR');
-
-    if (esCorreoValido) {
-        var correoRecuperacion = id("correoRecuperacion");
-
-        const data = new FormData();
-        data.append('correoRecuperacion', correoRecuperacion.value.trim());
-
-        fetch('../../dao/daoRecuperacionPassword.php', {
-            method: 'POST',
-            body: data
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Hubo un problema al recuperar la contraseña. Por favor, intenta de nuevo más tarde.');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.status === 'success') {
-                    console.log(data.message);
-                    Swal.fire({
-                        title: "Solicitud exitosa",
-                        text: "Hemos enviado un correo electrónico a "  + correoRecuperacion.value +" para restablecer tu contraseña.",
-                        icon: "success",
-                        confirmButtonText: "OK"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            window.location.href = "../sesion/indexSesion.php";
-                        }
-                    });
-                } else if (data.status === 'error') {
-                    console.log(data.message);
-                    Swal.fire({
-                        title: "Error",
-                        text: data.message,
-                        icon: "error",
-                        confirmButtonText: "OK"
-                    });
-                }
-            })
-            .catch(error => {
-                //console.error(error);
-                Swal.fire({
-                    title: "Error",
-                    text: "Hubo un problema al procesar tu solicitud. Por favor, intenta de nuevo más tarde.",
-                    icon: "error",
-                    confirmButtonText: "OK"
-                });
-            });
-    } else {
-        Swal.fire({
-            title: "Correo no válido",
-            text: "Revise su información",
-            icon: "error"
-        });
-    }
-}
 
 function showResult(result) {
     // Código para manejar el resultado exitoso
