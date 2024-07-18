7d39e67695bf6554178718f97e4aa059d5e35a68
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-18 11:43:35 -0600

    Actualizacion daoEstatusPrueba.php

--- PW_MetrologyLaboratory/dao/daoEstatusPrueba.php
+++ PW_MetrologyLaboratory/dao/daoEstatusPrueba.php
@@ -9,7 +9,9 @@ function ContadorEstatus(){
     $conex = $con->conectar();
 
     $datos = mysqli_query($conex, "SELECT id_estatusPrueba,descripcionEstatus 
-                                            FROM EstatusPrueba ORDER BY id_estatusPrueba;");
+                                           FROM EstatusPrueba 
+                                          WHERE id_estatusPrueba <> 6
+                                          ORDER BY id_estatusPrueba;");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
