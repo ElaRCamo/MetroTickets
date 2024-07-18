d1139dc7fc3696cfd73daf65ade336d3338fc9b7
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-18 10:00:48 -0600

    Actualizacion daoConsultaPruebasSolicitante.php

--- PW_MetrologyLaboratory/dao/daoConsultaPruebasSolicitante.php
+++ PW_MetrologyLaboratory/dao/daoConsultaPruebasSolicitante.php
@@ -24,11 +24,11 @@ function resumenPrueba($id_solicitante){
                     u_solic.nombreUsuario AS nombreSolic,
                     CASE
                         WHEN s.id_estatusPrueba = 1 
-                            THEN CONCAT('<span class=\"badge bg-secondary\">', ep.descripcionEstatus, '</span>')
+                            THEN CONCAT('<span class=\"badge bg-warning text-dark\">', ep.descripcionEstatus, '</span>')
                         WHEN s.id_estatusPrueba = 2 
                             THEN CONCAT('<span class=\"badge bg-primary\">', ep.descripcionEstatus, '</span>')
                         WHEN s.id_estatusPrueba = 3 
-                            THEN CONCAT('<span class=\"badge bg-warning text-dark\">', ep.descripcionEstatus, '</span>')
+                            THEN CONCAT('<span class=\"badge bg-info text-dark\">', ep.descripcionEstatus, '</span>')
                         WHEN s.id_estatusPrueba = 4 
                             THEN CONCAT('<span class=\"badge bg-success\">', ep.descripcionEstatus, '</span>')
                         WHEN s.id_estatusPrueba = 5 
