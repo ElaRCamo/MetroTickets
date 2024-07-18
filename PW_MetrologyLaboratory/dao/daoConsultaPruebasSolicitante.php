0f4716a44a75d8e96dec745b0ff405a42c9aef5a
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-18 09:03:40 -0600

    Actualizacion daoConsultaPruebasSolicitante.php

--- PW_MetrologyLaboratory/dao/daoConsultaPruebasSolicitante.php
+++ PW_MetrologyLaboratory/dao/daoConsultaPruebasSolicitante.php
@@ -11,36 +11,35 @@ function resumenPrueba($id_solicitante){
 
     $datosPrueba =  mysqli_query($conex,
         "SELECT
-                s.id_prueba,
-                s.fechaSolicitud,
-                s.fechaRespuesta,
-                s.fechaCompromiso,
-                ep.descripcionEstatus,
-                p.descripcionPrioridad,
-                s.id_tipoPrueba,
-                tp.descripcionPrueba,
-                s.especificaciones,
-                s.id_metrologo,
-                u_metro.nombreUsuario AS nombreMetro,
-                s.id_solicitante,
-                u_solic.nombreUsuario AS nombreSolic,
-                CASE 
-                    WHEN s.id_estatusPrueba = 1 THEN '<span class='label label-success'>Confirmado</span>'
-                    ELSE ep.descripcionEstatus
-                END AS estatusVisual
-            FROM
-                Pruebas s
-                LEFT JOIN Usuario u_metro ON s.id_metrologo = u_metro.id_usuario
-                LEFT JOIN Usuario u_solic ON s.id_solicitante = u_solic.id_usuario
-                LEFT JOIN TipoPrueba tp ON s.id_tipoPrueba = tp.id_tipoPrueba
-                LEFT JOIN EstatusPrueba ep ON s.id_estatusPrueba = ep.id_estatusPrueba
-                LEFT JOIN Prioridad p ON s.id_prioridad = p.id_prioridad
-            WHERE
-                s.id_solicitante = '$id_solicitante'
-                AND s.id_estatusPrueba <> 6
-            ORDER BY
-                s.id_prueba DESC;
-            ");
+                    s.id_prueba,
+                    s.fechaSolicitud,
+                    s.fechaRespuesta,
+                    s.fechaCompromiso,
+                    ep.descripcionEstatus,
+                    p.descripcionPrioridad,
+                    s.id_tipoPrueba,
+                    tp.descripcionPrueba,
+                    s.especificaciones,
+                    s.id_metrologo,
+                    u_metro.nombreUsuario AS nombreMetro,
+                    s.id_solicitante,
+                    u_solic.nombreUsuario AS nombreSolic,
+                    CASE 
+                        WHEN s.id_estatusPrueba = 1 THEN '<span class=''label label-success''>Confirmado</span>'
+                        ELSE ep.descripcionEstatus
+                    END AS estatusVisual
+                FROM
+                    Pruebas s
+                    LEFT JOIN Usuario u_metro ON s.id_metrologo = u_metro.id_usuario
+                    LEFT JOIN Usuario u_solic ON s.id_solicitante = u_solic.id_usuario
+                    LEFT JOIN TipoPrueba tp ON s.id_tipoPrueba = tp.id_tipoPrueba
+                    LEFT JOIN EstatusPrueba ep ON s.id_estatusPrueba = ep.id_estatusPrueba
+                    LEFT JOIN Prioridad p ON s.id_prioridad = p.id_prioridad
+                WHERE
+                    s.id_solicitante = '$id_solicitante'
+                    AND s.id_estatusPrueba <> 6
+                ORDER BY
+                    s.id_prueba DESC;");
 
     $resultado= mysqli_fetch_all($datosPrueba, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
