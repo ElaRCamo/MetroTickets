aa0e0df2f45c1cbabe489b85fd5bb6562142f383
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-19 09:10:42 -0600

    Se agrega tablaEstatusPiezas()

--- PW_MetrologyLaboratory/modules/review/modalResultados.php
+++ PW_MetrologyLaboratory/modules/review/modalResultados.php
@@ -3,7 +3,7 @@
     <div class="modal-lg modal-dialog modal-dialog-centered modal-dialog-scrollable ">
         <div class="modal-content">
             <div class="modal-header" id="resultadosModal">
-                <h5 class="modal-title" id="modalResultadosLabel">Responder solicitud </h5><br>
+                <h2 class="modal-title" id="modalResultadosLabel">Responder solicitud </h2><br>
                 <button type="button" class="btn-close" id="" data-bs-dismiss="modal" onclick="" aria-label="Close"></button>
             </div>
             <div class="modal-body row">
