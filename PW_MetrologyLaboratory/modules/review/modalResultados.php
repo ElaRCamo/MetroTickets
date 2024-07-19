c759d4093773f0641592f9596a45991ab5bbc547
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-07-19 11:42:29 -0600

    Se agrega tablaEstatusPiezas()

--- PW_MetrologyLaboratory/modules/review/modalResultados.php
+++ PW_MetrologyLaboratory/modules/review/modalResultados.php
@@ -114,7 +114,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="position-absolute top-50 start-50">
+                    <div class="position-absolute bottom-0 end-0">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="">Close</button>
                         <button type="button" class="btn btn-primary" id="btnGuardarResultados" onclick="validarResultados(id_review,id_user)" data-bs-dismiss="modal"><i class="las la-save"></i>Guardar</button>
                     </div>
