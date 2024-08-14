af56984f95e277a7c546bc33107e433266b089c9
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-08-14 09:56:40 -0600

    se actualiza function  updatePruebaAdmin

--- PW_MetrologyLaboratory/js/review.js
+++ PW_MetrologyLaboratory/js/review.js
@@ -503,6 +503,8 @@ function  updatePruebaAdmin(id_review, id_user, estatusPruebaAdmin,metrologoAdmi
             data.append(`reportes[${index}]`, newFile);
         }
 
+        alert("estatus: "+estatusPruebaAdmin.value + "file: "+newFile);
+
         if(estatusPruebaAdmin.value === '4' && newFile==="Sin resultados"){
             Swal.fire({
                 title: 'Error',
