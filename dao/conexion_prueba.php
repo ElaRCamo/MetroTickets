01716b3c6a5781cc21c562839fb0c4d5d26f157a
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-08-16 12:44:34 -0600

    Nuevo dominio

--- dao/conexion_prueba.php
+++ dao/conexion_prueba.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$conexion = mysqli_connect("127.0.0.1:3306","u543707098_MARIELA","Metrologia123","u543707098_PRODUCCION");
+$conexion = mysqli_connect("127.0.0.1:3306","u909553968_Ela","LMGrammer2024#","u909553968_MetroTickects");
 
 if($conexion){
     echo 'Conexión exitosa';
