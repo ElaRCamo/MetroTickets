d759ce992588a66722912f96a5673891e5ffe98b
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-08-19 10:06:58 -0600

    Nuevo dominio

--- dao/daoActualizarPruebaAdmin.php
+++ dao/daoActualizarPruebaAdmin.php
@@ -270,8 +270,8 @@ function ActualizarPiezas($conexUpdate, $numsParte, $estatussPiezas, $reportes, 
 
                 echo "existente reporte: ".$existingPieza['reportePieza']."\n";
                 echo "existente estatus: ".$existingPieza['id_estatus']."\n";
-                echo "nuevo reporte: ".$existingPieza['reporte']."\n";
-                echo "nuevo estatus: ".$existingPieza['estatusPieza']."\n";
+                echo "nuevo reporte: ".$pieza['reporte']."\n";
+                echo "nuevo estatus: ".$pieza['estatusPieza']."\n";
 
 
                 //Ya tiene un estatus de completado/pendiente por recoger y no se adjunta nuevo reporte: Se queda el mismo reporte
