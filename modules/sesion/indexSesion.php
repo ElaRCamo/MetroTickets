57be57f8394a01526037f1b081a9913922a28a3a
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-08-19 15:22:08 -0600

    Nuevo dominio

--- modules/sesion/indexSesion.php
+++ modules/sesion/indexSesion.php
@@ -29,7 +29,7 @@
         # Content section
             require_once('contentSesion.php');
         # Content section
-            require_once('../../footer.php')
+            //require_once('../../footer.php')
 
     ?>
 
