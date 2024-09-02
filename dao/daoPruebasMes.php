79a045075b8e6140fbe739c7dbdd65b16295be3f
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-09-02 12:07:56 -0600

    Actualización graficos

--- dao/daoPruebasMes.php
+++ dao/daoPruebasMes.php
@@ -2,16 +2,27 @@
 
 include_once('connection.php');
 
+session_start();
 $fecha_busqueda = date('Y-m') . '%';
 PruebasMesActual($fecha_busqueda);
 function PruebasMesActual($fecha_busqueda){
     $con = new LocalConector();
     $conex = $con->conectar();
+    $tipoUser = $_SESSION['tipoUsuario'];
+    $metrologo = $_SESSION['id_usuario'];
 
-    $datos = mysqli_query($conex, "SELECT COUNT(*)
+    if($tipoUser == 1){
+        $datos = mysqli_query($conex, "SELECT COUNT(*)
                                           FROM Pruebas
                                          WHERE fechaRespuesta LIKE '$fecha_busqueda'
-                                           AND id_estatusPrueba = 4;");
+                                           AND id_estatusPrueba IN  (4, 9);");
+    }else{
+        $datos = mysqli_query($conex, "SELECT COUNT(*)
+                                          FROM Pruebas
+                                         WHERE fechaRespuesta LIKE '$fecha_busqueda'
+                                           AND id_metrologo = '$metrologo'
+                                           AND id_estatusPrueba IN  (4, 9);");
+    }
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
