83f3f1ebd5d523ff65e77cafcd0ca61fac47c27d
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-09-02 13:09:46 -0600

    Se valida estatrus del usuario que se loguea

--- dao/login.php
+++ dao/login.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'daoUsuario.php';
 
 if(isset($_POST['iniciarSesionBtn'])){
@@ -7,7 +6,7 @@ if(isset($_POST['iniciarSesionBtn'])){
     session_start();
     $Nomina = $_POST['numNomina'];
 
-    if (strlen($Nomina) < 8) { //Validar los ceros (8 digitos)
+    if (strlen($Nomina) < 8) { // Validar los ceros (8 dígitos)
         $Nomina = str_pad($Nomina, 8, "0", STR_PAD_LEFT);
     }
 
@@ -16,12 +15,12 @@ if(isset($_POST['iniciarSesionBtn'])){
     if($resultado['success']){
         $_SESSION['numNomina'] = $Nomina;
         $_SESSION['password'] = $_POST['password'];
-        $_SESSION['nombreUsuario']= $resultado['nombreUsuario'];
-        $_SESSION['tipoUsuario']= $resultado['tipoUsuario'];
-        $_SESSION['nomina']= $resultado['idUser'];
-        $_SESSION['emailUsuario']= $resultado['emailUsuario'];
-        $_SESSION['fotoUsuario']= $resultado['foto'];
-        $_SESSION['estatus']= $resultado['estatus'];
+        $_SESSION['nombreUsuario'] = $resultado['nombreUsuario'];
+        $_SESSION['tipoUsuario'] = $resultado['tipoUsuario'];
+        $_SESSION['nomina'] = $resultado['idUser'];
+        $_SESSION['emailUsuario'] = $resultado['emailUsuario'];
+        $_SESSION['fotoUsuario'] = $resultado['foto'];
+        $_SESSION['estatus'] = $resultado['estatus'];
 
         $password_bd = $resultado['password_bd'];
         $tipoUsuario = $_SESSION['tipoUsuario'];
@@ -33,23 +32,52 @@ if(isset($_POST['iniciarSesionBtn'])){
             if($estatusUsuario == 1){
                 header("Location: ../index.php");
             }else{
-                echo "<script>alert('Sus credenciales estan desactivadas, debe contactar al administrador del laborattorio.')</script>";
+                echo "<script>
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Credenciales desactivadas',
+                        text: 'Sus credenciales están desactivadas, debe contactar al administrador del laboratorio.',
+                    }).then(function() {
+                        window.location.href = 'https://grammermx.com/Metrologia/MetroTickets/modules/sesion/indexSesion.php';
+                    });
+                </script>";
             }
         } else {
-            echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=https://grammermx.com/Metrologia/MetroTickets\modules\sesion\indexSesion.php'>";
-            echo "<script>alert('Contraseña incorrecta, verifique sus datos')</script>";
+            echo "<script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Contraseña incorrecta',
+                    text: 'Contraseña incorrecta, verifique sus datos.',
+                }).then(function() {
+                    window.location.href = 'https://grammermx.com/Metrologia/MetroTickets/modules/sesion/indexSesion.php';
+                });
+            </script>";
         }
-    }else{
-        echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=https://grammermx.com/Metrologia/MetroTickets/modules\sesion\indexSesion.php'>";
-        echo "<script>alert('El usuario no existe')</script>";
+    } else {
+        echo "<script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Usuario no encontrado',
+                text: 'El usuario no existe.',
+            }).then(function() {
+                window.location.href = 'https://grammermx.com/Metrologia/MetroTickets/modules/sesion/indexSesion.php';
+            });
+        </script>";
     }
 }
 
 if(isset($_POST['cerrarSesion']) || (isset($_POST['cerrarS']))){
     session_start();
     session_destroy();
-    echo "<script>alert('Sesión cerrada exitosamente')</script>";
-    echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=https://grammermx.com/Metrologia/MetroTickets/modules\sesion\indexSesion.php'>";
+    echo "<script>
+        Swal.fire({
+            icon: 'success',
+            title: 'Sesión finalizada',
+            text: 'Sesión cerrada exitosamente.',
+        }).then(function() {
+            window.location.href = 'https://grammermx.com/Metrologia/MetroTickets/modules/sesion/indexSesion.php';
+        });
+    </script>";
 }
 
 ?>
