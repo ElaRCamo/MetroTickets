9a5128959480363d40983405b3bf0add1a033d0e
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-07 14:43:11 -0600

    Add data for reports

--- modules/reports/reportePDF.php
+++ modules/reports/reportePDF.php
@@ -97,27 +97,27 @@ $css = file_get_contents("../../css/pdf.css");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
-<body>
+<body >
+
 <main>
     <div class="page-header row headerLogo">
-        <table class="table table-bordered table-hover table-sm table-responsive" id="datosGeneralesTablePDF">
-            <tbody>
-            <tr class="bg-primary">
-                <th>Pruebas realizadas:</th>
-                <td><?php echo $pruebasRealizadas; ?></td>
-                <th>Tiempo de respuesta</th>
-                <td><?php echo $tiempoPromedioRespuestaDias . " días/prueba"; ?></td>
+        <table id="tableTitle">
+            <tr class="">
+                <th class="">
+                    <div class="col divTitle" id="divRespdf">
+                        <h1>Reporte <?php echo obtenerNombreMes($mes).' '; echo $anio?></h1>
+                        <h6>LABORATORIO DE METROLOGÍA</h6>
+                        <?php echo "<small>Fecha: $date</small>";?>
+                    </div>
+                </th>
+                <td>
+                    <div class="col">
+                        <img class="logoGrammer2-img logoR" alt="LogoGrammer" src="https://grammermx.com/Metrologia/MetroTickets/imgs/logoGrammer.png"><br>
+                    </div>
+                </td>
             </tr>
-            <tr>
-                <th>Pruebas pendientes</th>
-                <td><?php echo $pruebasPendientes; ?></td>
-                <th>Eficiencia operativa</th>
-                <td><?php echo $eficienciaOperativa . " pruebas/día"; ?></td>
-            </tr>
-            </tbody>
         </table>
     </div>
-
     <div class="container-fluid" id="containerPruebaPDF" >
         <div class="row">
             <div class="table-responsive">
