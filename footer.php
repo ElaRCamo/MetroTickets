f6a7f836e1afaa5d509fef8ae0033515d5f0c8e9
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-11-13 12:15:31 -0600

    Add user manual

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
                 <small><a href="https://grammermx.com" target="_blank">© Grammer Querétaro.</a></small>
             </div>
             <div class="col-sm-4 text-center">
-                <small><a href="https://grammermx.com/Metrologia/MetroTickets/files/manual/Metrotickets.pdp" target="_blank">Soporte</a></small>
+                <small><a href="https://grammermx.com/Metrologia/MetroTickets/files/manual/Metrotickets.pdf" target="_blank">Soporte</a></small>
             </div>
         </div>
     </div>
